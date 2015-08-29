1edc4f2747c4cc8d4e3c06d72ec57063fff2a5f7
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2015-08-29 03:01:30 -0400

    Second found commit

--- Library/include/BitIO.h
+++ Library/include/BitIO.h
@@ -18,7 +18,11 @@
 extern "C" {
 #endif
     
-    typedef unsigned char uint24_t[3]; // unsigned 24 bit char array.
+    enum ShiftDirection {
+        Undefined = 0,
+        Right     = 1,
+        Left      = 2,
+    } ShiftDirection;
     
     enum Endian {
         Undetected = 0,
@@ -28,20 +32,21 @@ extern "C" {
     
     struct BitIO {
         // Common
-        enum Endian  NativeEndian; // SystemEndian
+        enum Endian          NativeEndian; // SystemEndian
         // Input
-        FILE                         *InputFP; // File to read from
-        DIR                         *InputDir; // Folder to read from
-        uint64_t                  InputFPSize; // How large is the input file in bytes?
-        uint8_t       InputBuffer[BufferSize]; // the actual InputBuffer
-        bool                     BufferIsFull; // tells if the entire buffer is full, or 0 padded.
-        uint64_t               InputBitOffset;
+        FILE                     *InputFP; // File to read from
+        DIR                     *InputDir; // Folder to read from
+        uint64_t              InputFPSize; // How large is the input file in bytes?
+        uint8_t   InputBuffer[BufferSize]; // the actual InputBuffer
+        uint64_t             BufferLength; // tells if the entire buffer is full, or 0 padded. bits or bytes?
+        uint64_t           InputBitOffset;
+        uint64_t       InputBitsRemaining;
         
         // Output
-        FILE                       *OutputFP; // File to write to
-        DIR                       *OutputDir;
-        uint8_t     OutputBuffer[BufferSize]; // Buffer
-        uint64_t             OutputBitOffset;
+        FILE                    *OutputFP; // File to write to
+        DIR                    *OutputDir;
+        uint8_t  OutputBuffer[BufferSize]; // Buffer
+        uint64_t          OutputBitOffset;
     } BitIO;
     
     struct UUID { /// TODO: Should I include room for hyphens?
@@ -61,19 +66,18 @@ extern "C" {
     int64_t PruneBits(int64_t Data2Prune);
     
     void OptionParser(int argc, char *argv, const char *OptString);
-    void UpdateBitsRead(uint64_t BitsRead);
     void SkipBits(uint64_t Bits2Skip);
     void Deallocate_BitIO(void);
     
     uint64_t FileSequence(int argc, char *argv, int WhichArg);
-    uint64_t InputBufferManager(uint64_t RequestedBits);
     uint64_t BufferManagment(uint64_t BitsRequested);
     uint64_t ReadRICE(int Bits2Read, bool StopBit);
     uint64_t CountBits(uint64_t Bits2Count);
     uint64_t WriteBits(uint64_t Bits2Write);
-    uint64_t ReadBits(uint64_t Bits2Read);
+    //uint64_t ReadBits(uint64_t Bits2Read);
     uint64_t Bytes2Bits(uint64_t Bytes);
     uint64_t Bits2Bytes(uint64_t Bits);
+    //uint64_t InputBufferManager(void);
     uint16_t EndianSwap16(uint16_t X);
     uint32_t EndianSwap32(uint32_t X);
     uint64_t EndianSwap64(uint64_t X);
