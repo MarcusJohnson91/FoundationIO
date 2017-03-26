7f7a6aadf841fce07c231b541dbccfda7df4b4b0
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2017-03-26 14:48:50 -0400

    Removed trailing whitespace
    Thanks Xcode Reindent!

--- Library/src/BufferIO.c
+++ Library/src/BufferIO.c
@@ -521,7 +521,7 @@ extern "C" {
         return false;
     }
     
-    uint32_t GenerateAdler32(const uint8_t *Data, const size_t DataSize) { 
+    uint32_t GenerateAdler32(const uint8_t *Data, const size_t DataSize) {
         uint32_t Adler  = 1;
         uint32_t Sum1   = Adler & 0xFFFF;
         uint32_t Sum2   = (Adler >> 16) & 0xFFFF;
@@ -533,7 +533,7 @@ extern "C" {
         return (Sum2 << 16) + Sum1;
     }
     
-    bool VerifyAdler32(const uint8_t *Data, const size_t DataSize, const uint32_t EmbeddedAdler32) { 
+    bool VerifyAdler32(const uint8_t *Data, const size_t DataSize, const uint32_t EmbeddedAdler32) {
         uint32_t GeneratedAdler32 = GenerateAdler32(Data, DataSize);
         if (GeneratedAdler32 != EmbeddedAdler32) {
             return false;
