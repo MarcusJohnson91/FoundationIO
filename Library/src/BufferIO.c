c80cd63bb05bf64463a6c0fe8f8ff6a5cb5daa79
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2017-05-15 07:56:00 -0400

    Help switch stuff

--- Library/src/BufferIO.c
+++ Library/src/BufferIO.c
@@ -397,7 +397,7 @@ extern "C" {
             Log(LOG_ERR, "libBitIO", "DisplayCMDHelp", "Pointer to CommandLineOptions is NULL\n");
         } else {
             printf("Options: (-|--|/)\n");
-            for (uint8_t SwitchNum = 0; SwitchNum < CMD->NumSwitches; SwitchNum++) {
+            for (uint8_t SwitchNum = 0; SwitchNum < CMD->NumSwitches - 1; SwitchNum++) {
                 printf("%s: %s\n", CMD->Switch[SwitchNum].Flag, CMD->Switch[SwitchNum].SwitchDescription);
                 /*
                  (-|--|/) Input: Input file or stdin with: -
