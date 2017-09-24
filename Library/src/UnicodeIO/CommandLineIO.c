1c1d545f0023231ccd3be25cd20e80b55687a59f
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2017-09-24 07:25:16 -0400

    Added GetCLIArgumentNumFromSwitchNum function to CommandLineIO

--- Library/src/UnicodeIO/CommandLineIO.c
+++ Library/src/UnicodeIO/CommandLineIO.c
@@ -424,6 +424,23 @@ extern "C" {
         return FoundSwitch;
     }
     
+    uint64_t GetCLIArgumentNumFromSwitchNum(CommandLineIO const *CLI, uint64_t SwitchNum) {
+        uint64_t FoundArgument = 0xFFFFFFFFFFFFFFFFULL;
+        if (CLI == NULL) {
+            Log(LOG_ERR, "libBitIO", "GetCLIArgumentNumFromSwitchNum", "Pointer to CommandLineIO is NULL");
+        } else if (SwitchNum > CLI->NumSwitches) {
+            Log(LOG_ERR, "libBitIO", "GetCLIArgumentNumFromSwitchNum", "SwitchNum %d is not a valid Switch", SwitchNum);
+        } else {
+            for (uint64_t Argument = 0ULL; Argument < CLI->NumArguments; Argument++) {
+                // Loop over the arguments until you find CLI->Arguments[Argument].SwitchNum that matches SwitchNum
+                if (CLI->Arguments[Argument].SwitchNum == SwitchNum) {
+                    FoundArgument = Argument;
+                }
+            }
+        }
+        return FoundArgument;
+    }
+    
     char *GetCLIArgumentResult(CommandLineIO const *CLI, const uint64_t ArgumentNum) {
         char *Result = NULL;
         if (CLI == NULL) {
