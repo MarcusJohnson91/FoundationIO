614ca47f7155d3ee9cc35c199699961393f0fc29
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2018-03-04 02:18:43 -0500

    Moved SwapEndian* functions to the Math file

--- Library/src/MathIO.c
+++ Library/src/MathIO.c
@@ -62,6 +62,21 @@ extern   "C" {
         return Bytes;
     }
     
+    inline uint16_t SwapEndian16(const uint16_t Data2Swap) {
+        return ((Data2Swap & 0xFF00) >> 8) | ((Data2Swap & 0x00FF) << 8);
+    }
+    
+    inline uint32_t SwapEndian32(const uint32_t Data2Swap) {
+        return ((Data2Swap & 0xFF000000) >> 24) | ((Data2Swap & 0x00FF0000) >> 8) | ((Data2Swap & 0x0000FF00) << 8) | ((Data2Swap & 0x000000FF) << 24);
+    }
+    
+    inline uint64_t SwapEndian64(const uint64_t Data2Swap) {
+        return (((Data2Swap & 0xFF00000000000000) >> 56) | ((Data2Swap & 0x00FF000000000000) >> 40) | \
+                ((Data2Swap & 0x0000FF0000000000) >> 24) | ((Data2Swap & 0x000000FF00000000) >>  8) | \
+                ((Data2Swap & 0x00000000FF000000) <<  8) | ((Data2Swap & 0x0000000000FF0000) << 24) | \
+                ((Data2Swap & 0x000000000000FF00) << 40) | ((Data2Swap & 0x00000000000000FF) << 56));
+    }
+    
 #ifdef   __cplusplus
 }
 #endif
