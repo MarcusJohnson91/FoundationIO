529e999a5b026849928a80e6ee02c3a7f9ff0e95
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2018-08-22 03:07:39 -0400

    StringTypes' UnknownFormat was changed to UnknownUnicodeFormat

--- Library/include/TextIO/StringIO.h
+++ Library/include/TextIO/StringIO.h
@@ -191,13 +191,13 @@ extern "C" {
     
     /*!
      @enum                StringTypes
-     @constant            UnknownFormat                        "Could be anything".
+     @constant            UnknownUnicodeFormat                 "Could be anything".
      @constant            UTF8Format                           "The string is UTF-8 encoded".
      @constant            UTF16Format                          "The string is UTF-15 encoded".
      @constant            UTF32Format                          "The string is UTF-32 encoded".
      */
     typedef enum StringTypes {
-                        UnknownFormat                          = 0,
+                        UnknownUnicodeFormat                   = 0,
                         UTF8Format                             = 1,
                         UTF16Format                            = 2,
                         UTF32Format                            = 3,
