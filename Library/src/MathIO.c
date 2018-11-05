aa6f49fcfe209ff3742f959b8c87caa71210f809
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2018-11-05 08:07:39 -0500

    Math: Pretty big overhaul, wrote some new functions, fixed and optimized some old algorithms, renamed some functions, lots of cleanup

--- Library/src/MathIO.c
+++ Library/src/MathIO.c
@@ -23,23 +23,23 @@ extern "C" {
         return X;
     }
     
-    uint64_t Absolutei(const int64_t Value) {
+    uint64_t AbsoluteI(const int64_t Value) {
        return (Value >= 0 ? (uint64_t) Value : (uint64_t) ~Value + 1);
     }
     
-    uint64_t Absolutef(const float Value) {
-        return (uint64_t) ExtractExponent(Value);
+    uint64_t AbsoluteF(const float Value) {
+        return (uint64_t) ExtractExponentF(Value);
     }
     
-    uint64_t Absoluted(const double Value) {
-        return (uint64_t) ExtractExponent(Value);
+    uint64_t AbsoluteD(const double Value) {
+        return (uint64_t) ExtractExponentD(Value);
     }
     
-    int64_t  Floorf(const float Value) {
+    int64_t  FloorF(const float Value) {
         int64_t  Result   = 0;
-        int8_t   Sign     = ExtractSign(Value);
-        int32_t  Mantissa = ExtractMantissa(Value);
-        int16_t  Exponent = ExtractExponent(Value);
+        int8_t   Sign     = ExtractSignF(Value);
+        int8_t   Exponent = ExtractExponentF(Value);
+        int32_t  Mantissa = ExtractMantissaF(Value);
         
         if (Mantissa == 0) {
             Result        =  Exponent * Sign;
@@ -53,11 +53,11 @@ extern "C" {
         return Result;
     }
     
-    int64_t  Floord(const double Value) {
+    int64_t  FloorD(const double Value) {
         int64_t  Result   = 0;
-        int8_t   Sign     = ExtractSign(Value);
-        int64_t  Mantissa = ExtractMantissa(Value);
-        int16_t  Exponent = ExtractExponent(Value);
+        int8_t   Sign     = ExtractSignD(Value);
+        int16_t  Exponent = ExtractExponentD(Value);
+        int64_t  Mantissa = ExtractMantissaD(Value);
         
         if (Mantissa == 0) {
             Result        =  Exponent * Sign;
@@ -71,11 +71,11 @@ extern "C" {
         return Result;
     }
     
-    int64_t  Ceilf(const float Value) {
+    int64_t  CeilF(const float Value) {
         int64_t  Result   = 0;
-        int8_t   Sign     = ExtractSign(Value);
-        int32_t  Mantissa = ExtractMantissa(Value);
-        int16_t  Exponent = ExtractExponent(Value);
+        int8_t   Sign     = ExtractSignF(Value);
+        int8_t   Exponent = ExtractExponentF(Value);
+        int32_t  Mantissa = ExtractMantissaF(Value);
         
         if (Mantissa == 0) {
             Result        =  Exponent * Sign;
@@ -89,11 +89,11 @@ extern "C" {
         return Result;
     }
     
-    int64_t  Ceild(const double Value) {
+    int64_t  CeilD(const double Value) {
         int64_t  Result   = 0;
-        int8_t   Sign     = ExtractSign(Value);
-        int64_t  Mantissa = ExtractMantissa(Value);
-        int16_t  Exponent = ExtractExponent(Value);
+        int8_t   Sign     = ExtractSignD(Value);
+        int16_t  Exponent = ExtractExponentD(Value);
+        int64_t  Mantissa = ExtractMantissaD(Value);
         
         if (Mantissa == 0) {
             Result        =  Exponent * Sign;
@@ -105,6 +105,140 @@ extern "C" {
             }
         }
         return Result;
+    }
+    
+    bool     DecimalIsNormalF(const float Decimal) {
+        bool IsNormal = No;
+        int16_t Exponent = ExtractExponentF(Decimal);
+        if (Exponent >= 1 && Exponent <= 0xFE) {
+            IsNormal  = Yes;
+        }
+        return IsNormal;
+    }
+    
+    bool     DecimalIsNormalD(const double Decimal) {
+        bool IsNormal = No;
+        int16_t Exponent = ExtractExponentD(Decimal);
+        if (Exponent >= 1 && Exponent <= 0x7FE) {
+            IsNormal  = Yes;
+        }
+        return IsNormal;
+    }
+    
+    bool     DecimalIsInfinityF(const float Decimal) {
+        bool    IsInfinity = No;
+        uint8_t Exponent   = (((uint32_t)Decimal) & 0x7F800000) >> 23;
+        int32_t Mantissa   = ExtractMantissaF(Decimal);
+        if (Exponent == 255 && Mantissa == 0) {
+            IsInfinity     = Yes;
+        }
+        return IsInfinity;
+    }
+    
+    bool     DecimalIsInfinityD(const double Decimal) {
+        bool    IsInfinity = No;
+        int16_t Exponent   = (((uint64_t)Decimal) & 0x7FF0000000000000) >> 52;
+        int32_t Mantissa   = ExtractMantissaD(Decimal);
+        if (Exponent == 2047 && Mantissa == 0) {
+            IsInfinity     = Yes;
+        }
+        return IsInfinity;
+    }
+    
+    bool     DecimalIsNotANumberF(const float Decimal) {
+        bool   IsNotANumber = No;
+        int8_t  Sign       = ExtractSignF(Decimal);
+        uint8_t Exponent   = (((uint32_t)Decimal) & 0x7F800000) >> 23;
+        int32_t Mantissa   = ExtractMantissaF(Decimal);
+        if (Sign == 1 && Exponent == 0xFF && Mantissa > 0) {
+            IsNotANumber   = Yes;
+        }
+        return IsNotANumber;
+    }
+    
+    bool     DecimalIsNotANumberD(const double Decimal) {
+        bool   IsNotANumber = No;
+        int8_t  Sign       = ExtractSignD(Decimal);
+        int16_t Exponent   = (((uint64_t)Decimal) & 0x7FF0000000000000) >> 52;
+        int32_t Mantissa   = ExtractMantissaD(Decimal);
+        if (Sign == 1 && Exponent == 0x7FF && Mantissa > 0) {
+            IsNotANumber   = Yes;
+        }
+        return IsNotANumber;
+    }
+    
+    bool     NumberHasDecimalPointF(const float Decimal) {
+        bool HasDecimalPoint = No;
+        int16_t Mantissa     = ExtractMantissaF(Decimal);
+        if ((Mantissa & 0x1FFF) > 0) {
+            HasDecimalPoint  = Yes;
+        }
+        return HasDecimalPoint;
+    }
+    
+    bool     NumberHasDecimalPointD(const double Decimal) {
+        bool HasDecimalPoint = No;
+        int16_t Mantissa     = ExtractMantissaD(Decimal);
+        if ((Mantissa & 0x7FFFFFFFFFF) > 0) {
+            HasDecimalPoint  = Yes;
+        }
+        return HasDecimalPoint;
+    }
+    
+    int8_t   ExtractSignI(const int64_t Sign2Extract) {
+        return (Sign2Extract & 0x8000000000000000) >> 63 == 1 ? -1 : 1;
+    }
+    
+    int8_t   ExtractSignF(const float Sign2Extract) {
+        uint32_t *Sign1 = (uint32_t *) &Sign2Extract;
+        uint32_t  Sign2 = (*Sign1 & 0x80000000) >> 31;
+        return Sign2 == 0 ? 1 : -1;
+    }
+    
+    int8_t   ExtractSignD(const double Sign2Extract) {
+        uint64_t *Sign1 = (uint64_t *) &Sign2Extract;
+        uint64_t  Sign2 = (*Sign1 & 0x8000000000000000) >> 63;
+        return Sign2 == 0 ? 1 : -1;
+    }
+    
+    int8_t  ExtractExponentF(const float Exponent2Extract) {
+        int8_t    Sign      = ExtractSignF(Exponent2Extract);
+        uint32_t *Exponent1 = (uint32_t *) &Exponent2Extract;
+        int8_t    Exponent2 = (*Exponent1 & 0x7F800000) >> 23;
+        int8_t    Exponent3 = (Exponent2 - 127) * Sign;
+        return Exponent3;
+    }
+    
+    int16_t  ExtractExponentD(const double Exponent2Extract) {
+        int8_t    Sign      = ExtractSignD(Exponent2Extract);
+        uint64_t *Exponent1 = (uint64_t *) &Exponent2Extract;
+        int16_t   Exponent2 = (*Exponent1 & 0x7FF0000000000000) >> 52;
+        int16_t   Exponent3 = (Exponent2 - 1023) * Sign;
+        return Exponent3;
+    }
+    
+    int32_t  ExtractMantissaF(const float Number2Extract) {
+        uint32_t *Mantissa1  = (uint32_t *) &Number2Extract;
+        int32_t   Mantissa2  = *Mantissa1 & 0x7FFFFFUL;
+        int32_t   Mantissa3  = 0UL;
+        if (DecimalIsNormalF(Number2Extract)) {
+            Mantissa3        = Mantissa2 | 0x800000;
+        } else {
+            Mantissa3        = Mantissa2;
+        }
+        return Mantissa3;
+    }
+    
+    int64_t  ExtractMantissaD(const double Number2Extract) {
+        uint64_t *Mantissa1  = (uint64_t *) &Number2Extract;
+        uint64_t  Mantissa2  = *Mantissa1 & 0xFFFFFFFFFFFFFULL;
+        int32_t   Mantissa3  = 0UL;
+        if (DecimalIsNormalD(Number2Extract)) {
+            Mantissa3        = Mantissa2 | 0x10000000000000;
+        } else {
+            Mantissa3        = Mantissa2;
+        }
+        return Mantissa3;
     }
     
     uint64_t Exponentiate(const uint64_t Base, const uint64_t Exponent) {
@@ -157,7 +291,7 @@ extern "C" {
     }
     
     int64_t  Bits2Bytes(const int64_t Bits, const bool RoundUp) {
-        uint64_t AbsoluteBits = Absolutei(Bits);
+        uint64_t AbsoluteBits = AbsoluteI(Bits);
         int64_t  Bytes        = 0ULL;
         if (RoundUp == Yes) {
             Bytes             = (AbsoluteBits >> 3) + 1;
@@ -183,7 +317,7 @@ extern "C" {
                 );
     }
     
-    uint8_t Bits2ExtractFromByte(uint64_t Offset) {
+    uint8_t Bits2ExtractFromByte(const uint64_t Offset) {
         return 8 - (Offset % 8);
     }
     
@@ -195,92 +329,6 @@ extern "C" {
         Mask = Exponentiate(2, NumBits2Select) << (8 - NumBits2Select);
 #endif
         return Mask;
-    }
-    
-    bool     NumberIsNormalF(float Decimal) {
-        bool IsNormal = No;
-        int16_t Exponent = ExtractExponent(Decimal);
-        if (Exponent >= 1 && Exponent <= 0xFE) {
-            IsNormal  = Yes;
-        }
-        return IsNormal;
-    }
-    
-    bool     NumberIsNormalD(double Decimal) {
-        bool IsNormal = No;
-        int16_t Exponent = ExtractExponent(Decimal);
-        if (Exponent >= 1 && Exponent <= 0x7FE) {
-            IsNormal  = Yes;
-        }
-        return IsNormal;
-    }
-    
-    bool     NumberHasDecimalPointF(float Decimal) { // IDK what to call this, basically anything less than 0x1FFF
-        bool HasDecimalPoint = No;
-        int16_t Mantissa = ExtractMantissa(Decimal);
-        if ((Mantissa & 0x1FFF) > 0) {
-            HasDecimalPoint  = Yes;
-        }
-        return HasDecimalPoint;
-    }
-    
-    bool     NumberHasDecimalPointD(float Decimal) { // IDK what to call this, basically anything less than 0x1FFF
-        bool HasDecimalPoint = No;
-        int16_t Mantissa = ExtractMantissa(Decimal);
-        if ((Mantissa & 0x7FFFFFFFFFF) > 0) {
-            HasDecimalPoint  = Yes;
-        }
-        // 1025   = 0x4090040000000000
-        // 1024.5 = 0x4090020000000000
-        
-        // 0x40000000000 = Where the decimal point starts, that's the highest set decimal requiring bit.
-        
-        // Anything in this group of bits requires a decimal point: 0x7FFFFFFFFFF
-        return HasDecimalPoint;
-    }
-    
-    int8_t   ExtractSignI(int64_t Sign2Extract) {
-        return (Sign2Extract & 0x8000000000000000) >> 63 == 1 ? -1 : 1;
-    }
-    
-    int8_t   ExtractSignF(float Sign2Extract) {
-        uint32_t *Sign1 = (uint32_t *) &Sign2Extract;
-        uint32_t  Sign2 = (*Sign1 & 0x80000000) >> 31;
-        return Sign2 == 0 ? 1 : -1;
-    }
-    
-    int8_t   ExtractSignD(double Sign2Extract) {
-        uint64_t *Sign1 = (uint64_t *) &Sign2Extract;
-        uint64_t  Sign2 = (*Sign1 & 0x8000000000000000) >> 63;
-        return Sign2 == 0 ? 1 : -1;
-    }
-    
-    int16_t  ExtractExponentF(float Exponent2Extract) { // 1024.25 = 0x44800800
-        int8_t    Sign      = ExtractSign(Exponent2Extract);
-        uint32_t *Exponent1 = (uint32_t *) &Exponent2Extract;
-        int16_t   Exponent2 = (*Exponent1 & 0x7F800000) >> 23;
-        int16_t   Exponent3 = Exponent2 - 127;
-        return Exponent3;
-    }
-    
-    int16_t  ExtractExponentD(double Exponent2Extract) {
-        int8_t    Sign      = ExtractSign(Exponent2Extract);
-        uint64_t *Exponent1 = (uint64_t *) &Exponent2Extract;
-        int16_t   Exponent2 = (*Exponent1 & 0x7FF0000000000000) >> 52;
-        int16_t   Exponent3 = Exponent2 - 1023;
-        return Exponent3;
-    }
-    
-    int32_t  ExtractMantissaF(float Number2Extract) { // 0x44800800
-        uint32_t *Mantissa1  = (uint32_t *) &Number2Extract;
-        int32_t   Mantissa2  = *Mantissa1 & 0x7FFFFFUL;
-        return (Mantissa2 | 0x800000);
-    }
-    
-    int64_t  ExtractMantissaD(double Number2Extract) {
-        uint64_t *Mantissa1  = (uint64_t *) &Number2Extract;
-        uint64_t  Mantissa2  = *Mantissa1 & 0xFFFFFFFFFFFFFULL;
-        return (Mantissa2 | 0x10000000000000);
     }
     
 #ifdef __cplusplus
