cae1f2c2b938730d8b9daa52f209ee65c55f5081
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2018-12-30 09:43:39 -0500

    Test_StringIO: reorganized the test

--- Tests/Test_StringIO.c
+++ Tests/Test_StringIO.c
@@ -7,14 +7,23 @@
 extern "C" {
 #endif
     
-    int main(int argc, const char *argv[]) {
+    
+    bool UTF8_BasicTest(void) {
+        bool      TestPassed     = true;
         UTF8      TestString8[]  = U8("Size: 7");
         UTF32    *TestString32   = UTF8_Decode(TestString8);
         uint64_t  TestStringSize = UTF32_GetStringSizeInCodePoints(TestString32);
         if (TestStringSize != 7) {
+            TestPassed           = false;
             Log(Log_TEST, __func__, U8("String \"%s\" is supposed to be 7 CodePoints long, but is actually %llu"), TestString8, TestStringSize);
         }
-        return 0;
+        return TestPassed;
+    }
+    
+    int main(int argc, const char *argv[]) {
+        bool TestSuitePassed = true;
+        
+        return TestSuitePassed;
     }
     
 #ifdef __cplusplus
