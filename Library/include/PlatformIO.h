897a875dc50b2f291b06aaeb8b220a12811e64e5
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2019-01-03 23:45:06 -0500

    Macros: Removed header version of FoundationIO_GetNumCPUCores

--- Library/include/PlatformIO.h
+++ Library/include/PlatformIO.h
@@ -373,22 +373,7 @@ extern "C" {
                                 NULLTerminatorSize         = 1,
     } FoundationIOConstants;
     
-    uint64_t FoundationIO_GetNumCPUCores() {
-        uint64_t NumCPUCores = 0ULL;
-#if   (FoundationIOTargetOS == FoundationIOOSPOSIX)
-        int SysInfo[2]  = {CTL_HW, HW_AVAILCPU};
-        size_t   Length = 8;
-        sysctl(SysInfo, 2, &NumCPUCores, &Length, NULL, 0);
-        if (NumCPUCores < 1) {
-            NumCPUCores = 1;
-        }
-#elif (FoundationIOTargetOS == FoundationIOOSWindows)
-        SYSTEM_INFO WinSysInfo;
-        GetSystemInfo(&WinSysInfo);
-        NumCPUCores = WinSysInfo.dwNumberOfProcessors;
-#endif
-        return NumCPUCores;
-    }
+    uint64_t FoundationIO_GetNumCPUCores(void);
     
 #ifdef __cplusplus
 }
