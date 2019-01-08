5bddbe02dd0f7b3ada7992737095e1141be50a60
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2019-01-08 13:19:06 -0500

    Tiny tweaks to the tests

--- Tests/Test_ContainerIO.c
+++ Tests/Test_ContainerIO.c
@@ -1,6 +1,5 @@
 #include "../libFoundationIO/include/TestIO.h"
 #include "../libFoundationIO/include/Log.h"
-
 #include "../libFoundationIO/include/ContainerIO.h"
 
 #ifdef __cplusplus
@@ -8,10 +7,11 @@ extern "C" {
 #endif
     
     int main(int argc, const char *argv[]) {
-        ImageContainer *Image = ImageContainer_Init(ImageType_Integer8, 8, 2, ImageMask_Red | ImageMask_Green | ImageMask_Blue, 3840, 2160);
+        ImageContainer *Image = ImageContainer_Init(ImageType_Integer8, ImageMask_Red | ImageMask_Green | ImageMask_Blue, 3840, 2160);
+        
         ImageHistogram *Hist  = ImageHistogram_Init(Image);
         
-        AudioContainer *Audio = AudioContainer_Init(AudioType_Unsigned | AudioType_Integer16, 24, AudioMask_FrontLeft | AudioMask_FrontRight, 48000, 4096);
+        AudioContainer *Audio = AudioContainer_Init(AudioType_Unsigned | AudioType_Integer32, AudioMask_FrontLeft | AudioMask_FrontRight, 48000, 4096);
         return 0;
     }
     
--- Tests/Test_StringIO.c
+++ Tests/Test_StringIO.c
@@ -1,6 +1,5 @@
 #include "../libFoundationIO/include/TestIO.h"
 #include "../libFoundationIO/include/Log.h"
-
 #include "../libFoundationIO/include/StringIO.h"
 
 #ifdef __cplusplus
@@ -17,6 +16,17 @@ extern "C" {
             TestPassed           = false;
             Log(Log_TEST, __func__, U8("String \"%s\" is supposed to be 7 CodePoints long, but is actually %llu"), TestString8, TestStringSize);
         }
+        return TestPassed;
+    }
+    
+    bool UTF8_TrimStringTest(void) {
+        bool TestPassed = true;
+        /*
+         Create a string with whitespace that needs to be cut down to size
+         
+         Example String: "   Lil' Wayne feat Eminem   "
+         I want all the leading and trailing spaces to be removed, but not the ones in the body
+         */
         return TestPassed;
     }
     
