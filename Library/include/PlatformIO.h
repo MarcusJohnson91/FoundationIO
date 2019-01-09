2b6f289ae1d6271c1798605c138241ce635c8472
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2019-01-09 13:08:20 -0500

    Macros cleanup

--- Library/include/PlatformIO.h
+++ Library/include/PlatformIO.h
@@ -64,22 +64,6 @@ extern "C" {
 #define             FoundationIOTargetOS FoundationIOOSMacClassic
 #endif           /* FoundationIOTargetOS */
     
-#ifndef             NewLineWithNULLSize
-#define             NewLineWithNULLSize    1
-#endif
-    
-#ifndef             NewLineUTF8
-#define             NewLineUTF8  u8"\r"
-#endif
-    
-#ifndef             NewLineUTF16
-#define             NewLineUTF16  u"\r"
-#endif
-    
-#ifndef             NewLineUTF32
-#define             NewLineUTF32  U"\r"
-#endif
-    
 #elif (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__ANDROID__) || defined(__minix) || defined(__linux__) || defined(__unix__) || defined(_POSIX_C_SOURCE)
     
 #include <dlfcn.h>      /* Included for shared library support */
@@ -88,22 +72,6 @@ extern "C" {
 #include <unistd.h>     /* Included for stdin/stdout/stderr */
     
 #define FoundationIOTargetOS (FoundationIOOSPOSIX)
-    
-#ifndef             NewLineWithNULLSize
-#define             NewLineWithNULLSize    1
-#endif
-    
-#ifndef             NewLineUTF8
-#define             NewLineUTF8  u8"\n"
-#endif
-    
-#ifndef             NewLineUTF16
-#define             NewLineUTF16  u"\n"
-#endif
-    
-#ifndef             NewLineUTF32
-#define             NewLineUTF32  U"\n"
-#endif
     
 #ifndef             typeof
 #define             typeof(Variable)                                                           __typeof__(Variable)
@@ -192,22 +160,6 @@ extern "C" {
 #define             FoundationIOTargetOS FoundationIOOSWindows
 #endif
     
-#ifndef             NewLineWithNULLSize
-#define             NewLineWithNULLSize    2
-#endif
-    
-#ifndef             NewLineUTF8
-#define             NewLineUTF8  u8"\r\n"
-#endif
-    
-#ifndef             NewLineUTF16
-#define             NewLineUTF16  u"\r\n"
-#endif
-    
-#ifndef             NewLineUTF32
-#define             NewLineUTF32  U"\r\n"
-#endif
-    
 #ifndef             typeof
 #define             typeof(Variable)                                                           __typeof(Variable)
 #endif
@@ -292,61 +244,83 @@ extern "C" {
     
 #endif   /* End OS detection */
     
-#ifndef FoundationIOCompileTimeByteOrderUnknown
-#define FoundationIOCompileTimeByteOrderUnknown                                                0
+#ifndef             FoundationIOCompileTimeByteOrderUnknown
+#define             FoundationIOCompileTimeByteOrderUnknown                (0)
 #endif
     
-#ifndef FoundationIOCompileTimeByteOrderBE
-#define FoundationIOCompileTimeByteOrderBE                                                     1
+#ifndef             FoundationIOCompileTimeByteOrderBE
+#define             FoundationIOCompileTimeByteOrderBE                     (1)
 #endif
     
-#ifndef FoundationIOCompileTimeByteOrderLE
-#define FoundationIOCompileTimeByteOrderLE                                                     2
+#ifndef             FoundationIOCompileTimeByteOrderLE
+#define             FoundationIOCompileTimeByteOrderLE                     (2)
 #endif
     
-#ifndef FoundationIOCompilerIsUnknown
-#define FoundationIOCompilerIsUnknown                                                          0
+#ifndef             FoundationIOCompilerIsUnknown
+#define             FoundationIOCompilerIsUnknown                          (0)
 #endif
     
-#ifndef FoundationIOCompilerIsClang
-#define FoundationIOCompilerIsClang                                                            1
+#ifndef             FoundationIOCompilerIsClang
+#define             FoundationIOCompilerIsClang                            (1)
 #endif
     
-#ifndef FoundationIOCompilerIsMSVC
-#define FoundationIOCompilerIsMSVC                                                             2
+#ifndef             FoundationIOCompilerIsMSVC
+#define             FoundationIOCompilerIsMSVC                             (2)
 #endif
     
-#ifndef FoundationIOCompilerIsGCC
-#define FoundationIOCompilerIsGCC                                                              3
+#ifndef             FoundationIOCompilerIsGCC
+#define             FoundationIOCompilerIsGCC                              (3)
 #endif
     
-#ifndef FoundationIOCompiler
+#ifndef             FoundationIOCompiler
 #if   defined(__clang__)
-#define FoundationIOCompiler (FoundationIOCompilerIsClang)
+#define             FoundationIOCompiler                                   (FoundationIOCompilerIsClang)
 #elif defined(_MSC_VER)
-#define FoundationIOCompiler (FoundationIOCompilerIsMSVC)
+#define             FoundationIOCompiler                                   (FoundationIOCompilerIsMSVC)
 #elif defined(__GNUC__) || defined(__GNUG__)
-#define FoundationIOCompiler (FoundationIOCompilerIsGCC)
+#define             FoundationIOCompiler                                   (FoundationIOCompilerIsGCC)
 #endif
 #endif /* FoundationIOCompiler */
     
-#if   (FoundationIOTargetOS == FoundationIOOSPOSIX)
+#if   (FoundationIOCompiler == FoundationIOCompilerIsClang || FoundationIOCompiler == FoundationIOCompilerIsGCC)
 #if   (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
-#define FoundationIOTargetByteOrder  (FoundationIOCompileTimeByteOrderBE)
+#define             FoundationIOTargetByteOrder                            (FoundationIOCompileTimeByteOrderBE)
 #elif (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
-#define FoundationIOTargetByteOrder (FoundationIOCompileTimeByteOrderLE)
+#define             FoundationIOTargetByteOrder                            (FoundationIOCompileTimeByteOrderLE)
 #endif /* __BYTE_ORDER__ */
-#elif (FoundationIOTargetOS == FoundationIOOSWindows)
-#define FoundationIOTargetByteOrder (FoundationIOCompileTimeByteOrderLE)
-#endif /* FoundationIOTargetOS */
+#elif (FoundationIOCompiler == FoundationIOCompilerIsMSVC)
+#define             FoundationIOTargetByteOrder                            (FoundationIOCompileTimeByteOrderLE)
+#endif /* FoundationIOCompiler */
     
-    /*!
-     @enum                      FoundationIOConstants
-     @constant                  NULLTerminatorSize         "The size of a NULL terminator".
-     */
-    typedef enum FoundationIOConstants {
-                                NULLTerminatorSize         = 1,
-    } FoundationIOConstants;
+#ifndef NewLineUTF8
+#if   (FoundationIOTargetOS == FoundationIOOSPOSIX)
+#define             NewLineUTF8                                            (u8"\n")
+#elif (FoundationIOTargetOS == FoundationIOOSWindows)
+#define             NewLineUTF8                                            (u8"\r\n")
+#elif (FoundationIOTargetOS == FoundationIOOSMacClassic)
+#define             NewLineUTF8                                            (u8"\r")
+#endif /* TargetOS */
+#endif /* Ifndef NewLineUTF8 */
+    
+#ifndef NewLineUTF16
+#if   (FoundationIOTargetOS == FoundationIOOSPOSIX)
+#define             NewLineUTF16                                           (u"\n")
+#elif (FoundationIOTargetOS == FoundationIOOSWindows)
+#define             NewLineUTF16                                           (u"\r\n")
+#elif (FoundationIOTargetOS == FoundationIOOSMacClassic)
+#define             NewLineUTF16                                           (u"\r")
+#endif /* TargetOS */
+#endif /* Ifndef NewLineUTF16 */
+    
+#ifndef NewLineUTF32
+#if   (FoundationIOTargetOS == FoundationIOOSPOSIX)
+#define             NewLineUTF32                                           (U"\n")
+#elif (FoundationIOTargetOS == FoundationIOOSWindows)
+#define             NewLineUTF32                                           (U"\r\n")
+#elif (FoundationIOTargetOS == FoundationIOOSMacClassic)
+#define             NewLineUTF32                                           (U"\r")
+#endif /* TargetOS */
+#endif /* Ifndef NewLineUTF32 */
     
     uint64_t FoundationIO_GetNumCPUCores(void);
     
