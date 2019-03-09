6b2a86180485a8d56234486acf2eb773d05be126
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2019-03-08 23:09:04 -0500

    Math: Made all function parameters const
    Also, removed GetNumBitsToRepresentValue because GetNumDigitsInBase does the same thing and supports alternative bases

--- Library/src/MathIO.c
+++ Library/src/MathIO.c
@@ -6,15 +6,15 @@
 extern "C" {
 #endif
     
-    bool IsNegative(int64_t Integer) {
+    bool IsNegative(const int64_t Integer) {
         return (Integer & 0x8000000000000000) >> 63;
     }
     
-    bool IsOdd(int64_t Integer) {
+    bool IsOdd(const int64_t Integer) {
         return Integer & 1;
     }
     
-    uint64_t AbsoluteI(int64_t Integer) {
+    uint64_t AbsoluteI(const int64_t Integer) {
         bool Sign = Integer >> 63;
         return (Integer ^ -Sign) + Sign;
     }
@@ -29,35 +29,35 @@ extern "C" {
         uint64_t  Integer;
     } Double2Integer;
     
-    uint32_t ConvertFloat2Integer(float Decimal) {
+    uint32_t ConvertFloat2Integer(const float Decimal) {
         Float2Integer Integer = {.Float = Decimal};
         return Integer.Integer;
     }
     
-    uint64_t ConvertDouble2Integer(double Decimal) {
+    uint64_t ConvertDouble2Integer(const double Decimal) {
         Double2Integer Integer = {.Float = Decimal};
         return Integer.Integer;
     }
     
-    float ConvertInteger2Float(uint32_t Integer) {
+    float ConvertInteger2Float(const uint32_t Integer) {
         Float2Integer Float = {.Integer = Integer};
         return Float.Float;
     }
     
-    double ConvertInteger2Double(uint64_t Integer) {
+    double ConvertInteger2Double(const uint64_t Integer) {
         Double2Integer Float = {.Integer = Integer};
         return Float.Float;
     }
     
-    uint8_t AbsoluteF(float Decimal) {
+    uint8_t AbsoluteF(const float Decimal) {
         return (uint8_t) ExtractExponentF(Decimal);
     }
     
-    uint16_t AbsoluteD(double Decimal) {
+    uint16_t AbsoluteD(const double Decimal) {
         return (uint16_t) ExtractExponentD(Decimal);
     }
     
-    int64_t  FloorF(float Decimal) {
+    int64_t  FloorF(const float Decimal) {
         int64_t  Result   = 0;
         int8_t   Sign     = ExtractSignF(Decimal);
         int8_t   Exponent = ExtractExponentF(Decimal);
@@ -75,7 +75,7 @@ extern "C" {
         return Result;
     }
     
-    int64_t  FloorD(double Decimal) {
+    int64_t  FloorD(const double Decimal) {
         int64_t  Result   = 0;
         int8_t   Sign     = ExtractSignD(Decimal);
         int16_t  Exponent = ExtractExponentD(Decimal);
@@ -93,7 +93,7 @@ extern "C" {
         return Result;
     }
     
-    int64_t  CeilF(float Decimal) {
+    int64_t  CeilF(const float Decimal) {
         int64_t  Result   = 0;
         int8_t   Sign     = ExtractSignF(Decimal);
         int8_t   Exponent = ExtractExponentF(Decimal);
@@ -111,7 +111,7 @@ extern "C" {
         return Result;
     }
     
-    int64_t  CeilD(double Decimal) {
+    int64_t  CeilD(const double Decimal) {
         int64_t  Result   = 0;
         int8_t   Sign     = ExtractSignD(Decimal);
         int16_t  Exponent = ExtractExponentD(Decimal);
@@ -129,7 +129,7 @@ extern "C" {
         return Result;
     }
     
-    int64_t  RoundF(float Decimal) {
+    int64_t  RoundF(const float Decimal) {
         int64_t  Result   = 0;
         int8_t   Sign     = ExtractSignF(Decimal);
         int8_t   Exponent = ExtractExponentF(Decimal);
@@ -142,7 +142,7 @@ extern "C" {
         return Result * Sign;
     }
     
-    int64_t  RoundD(double Decimal) {
+    int64_t  RoundD(const double Decimal) {
         int64_t  Result   = 0;
         int8_t   Sign     = ExtractSignD(Decimal);
         int16_t  Exponent = ExtractExponentD(Decimal);
@@ -155,15 +155,15 @@ extern "C" {
         return Result * Sign;
     }
     
-    int64_t  Min(int64_t Integer1, int64_t Integer2) {
+    int64_t  Min(const int64_t Integer1, const int64_t Integer2) {
         return Integer2 ^ ((Integer1 ^ Integer2) & -(Integer1 < Integer2));
     }
     
-    int64_t  Max(int64_t Integer1, int64_t Integer2) {
+    int64_t  Max(const int64_t Integer1, const int64_t Integer2) {
         return Integer1 ^ ((Integer1 ^ Integer2) & -(Integer1 < Integer2));
     }
     
-    bool     DecimalIsNormalF(float Decimal) {
+    bool     DecimalIsNormalF(const float Decimal) {
         bool   IsNormal = No;
         int8_t Exponent = ExtractExponentF(Decimal);
         if (Exponent >= 1 && Exponent <= 0x7E) {
@@ -172,7 +172,7 @@ extern "C" {
         return IsNormal;
     }
     
-    bool     DecimalIsNormalD(double Decimal) {
+    bool     DecimalIsNormalD(const double Decimal) {
         bool IsNormal = No;
         int16_t Exponent = ExtractExponentD(Decimal);
         if (Exponent >= 1 && Exponent <= 0x7FE) {
@@ -181,7 +181,7 @@ extern "C" {
         return IsNormal;
     }
     
-    bool     DecimalIsInfinityF(float Decimal) {
+    bool     DecimalIsInfinityF(const float Decimal) {
         bool    IsInfinity = No;
         uint8_t Exponent   = (((uint32_t)Decimal) & 0x7F800000) >> 23;
         int32_t Mantissa   = ExtractMantissaF(Decimal);
@@ -191,7 +191,7 @@ extern "C" {
         return IsInfinity;
     }
     
-    bool     DecimalIsInfinityD(double Decimal) {
+    bool     DecimalIsInfinityD(const double Decimal) {
         bool    IsInfinity = No;
         int16_t Exponent   = (((uint64_t)Decimal) & 0x7FF0000000000000) >> 52;
         int32_t Mantissa   = ExtractMantissaD(Decimal);
@@ -201,7 +201,7 @@ extern "C" {
         return IsInfinity;
     }
     
-    bool     DecimalIsNotANumberF(float Decimal) {
+    bool     DecimalIsNotANumberF(const float Decimal) {
         bool   IsNotANumber = No;
         int8_t  Sign       = ExtractSignF(Decimal);
         uint8_t Exponent   = (((uint32_t)Decimal) & 0x7F800000) >> 23;
@@ -212,7 +212,7 @@ extern "C" {
         return IsNotANumber;
     }
     
-    bool     DecimalIsNotANumberD(double Decimal) {
+    bool     DecimalIsNotANumberD(const double Decimal) {
         bool   IsNotANumber = No;
         int8_t  Sign       = ExtractSignD(Decimal);
         int16_t Exponent   = (((uint64_t)Decimal) & 0x7FF0000000000000) >> 52;
@@ -223,7 +223,7 @@ extern "C" {
         return IsNotANumber;
     }
     
-    bool     NumberHasDecimalPointF(float Decimal) {
+    bool     NumberHasDecimalPointF(const float Decimal) {
         bool HasDecimalPoint = No;
         int32_t Mantissa     = ExtractMantissaF(Decimal);
         if ((Mantissa & 0x1FFF) > 0) {
@@ -232,7 +232,7 @@ extern "C" {
         return HasDecimalPoint;
     }
     
-    bool     NumberHasDecimalPointD(double Decimal) {
+    bool     NumberHasDecimalPointD(const double Decimal) {
         bool HasDecimalPoint = No;
         int64_t Mantissa     = ExtractMantissaD(Decimal);
         if ((Mantissa & 0x7FFFFFFFFFF) > 0) {
@@ -241,88 +241,87 @@ extern "C" {
         return HasDecimalPoint;
     }
     
-    int8_t   ExtractSignI(int64_t Integer) {
+    int8_t   ExtractSignI(const int64_t Integer) {
         return (Integer & 0x8000000000000000) >> 63 == 1 ? -1 : 1;
     }
     
-    int8_t   ExtractSignF(float Decimal) {
+    int8_t   ExtractSignF(const float Decimal) {
         uint32_t Integer       = ConvertFloat2Integer(Decimal);
         int8_t   Sign          = (Integer & 0x80000000) >> 31;
         return   Sign == 0 ? 1 : -1;
     }
     
-    int8_t   ExtractSignD(double Decimal) {
+    int8_t   ExtractSignD(const double Decimal) {
         uint64_t Integer        = ConvertDouble2Integer(Decimal);
         int8_t Sign             = (Integer & 0x8000000000000000) >> 63;
         return Sign == 0 ? 1 : -1;
     }
     
-    int8_t  ExtractExponentF(float Decimal) {
+    int8_t  ExtractExponentF(const float Decimal) {
         uint32_t Integer       = ConvertFloat2Integer(Decimal);
         int8_t   Sign          = ExtractSignF(Decimal);
         int8_t   Exponent      = (Integer & 0x7F800000) >> 23;
         return (Exponent - 127) * Sign;
     }
     
-    int16_t  ExtractExponentD(double Decimal) {
+    int16_t  ExtractExponentD(const double Decimal) {
         uint64_t Integer        = ConvertDouble2Integer(Decimal);
         int8_t   Sign           = ExtractSignD(Decimal);
         int16_t  Exponent       = (Integer & 0x7FF0000000000000) >> 52;
         return (Exponent - 1023) * Sign;
     }
     
-    int32_t  ExtractMantissaF(float Decimal) {
+    int32_t  ExtractMantissaF(const float Decimal) {
         uint32_t Integer       = ConvertFloat2Integer(Decimal);
         uint32_t Mantissa      = Integer & 0x7FFFFFUL;
         return DecimalIsNormalF(Decimal) ? Mantissa |= 0x800000 : Mantissa;
     }
     
-    int64_t  ExtractMantissaD(double Decimal) {
+    int64_t  ExtractMantissaD(const double Decimal) {
         uint64_t Integer        = ConvertDouble2Integer(Decimal);
         uint64_t Mantissa       = Integer & 0xFFFFFFFFFFFFFULL;
         return DecimalIsNormalF(Decimal) ? Mantissa |= 0x10000000000000 : Mantissa;
     }
     
-    float InsertSignF(float Insertee, int8_t Sign) {
+    float InsertSignF(const float Insertee, const int8_t Sign) {
         Float2Integer Insertee2 = {.Float = Insertee};
-        int8_t Sign2            = Sign == -1 ? 1 : 0;
-        Insertee2.Integer      &= (Sign2 << 31);
+        Insertee2.Integer      &= Sign == -1 ? 0x80000000 : 0;
         return Insertee2.Float;
     }
     
-    double InsertSignD(double Insertee, int8_t Sign) {
+    double InsertSignD(const double Insertee, const int8_t Sign) {
         Double2Integer Insertee2 = {.Float = Insertee};
-        Insertee2.Integer       &= (Sign << 63);
+        Insertee2.Integer       &= Sign == -1 ? 0x8000000000000000 : 0;
         return Insertee2.Float;
     }
     
-    float InsertExponentF(float Insertee, int8_t Value) {
+    float InsertExponentF(const float Insertee, const int8_t Value) {
         uint32_t Value2         = Value;
         Float2Integer Insertee2 = {.Float = Insertee};
         Insertee2.Integer      &= ((Value2 & 0xFF) << 23); // Value is 8 bits, and needs to be normalized to +/-
         return Insertee2.Float;
     }
     
-    double InsertExponentD(double Insertee, int16_t Value) {
+    double InsertExponentD(const double Insertee, const int16_t Value) {
         uint64_t Value2          = Value;
         Double2Integer Insertee2 = {.Float = Insertee};
         Insertee2.Integer       &= ((Value2 & 0x7FF) << 52);
         return Insertee2.Float;
     }
     
-    float InsertMantissaF(float Insertee, uint32_t Value) {
+    float InsertMantissaF(const float Insertee, const uint32_t Value) {
         Float2Integer Insertee2 = {.Float = Insertee};
         Insertee2.Integer      &= (Value & 0x7FFFFFUL); // Value is 8 bits, and needs to be normalized to +/-
         return Insertee2.Float;
     }
     
-    double InsertMantissaD(double Insertee, uint64_t Value) {
+    double InsertMantissaD(const double Insertee, const uint64_t Value) {
         Double2Integer Insertee2 = {.Float = Insertee};
         Insertee2.Integer       &= (Value & 0xFFFFFFFFFFFFFULL);
         return Insertee2.Float;
     }
     
-    uint64_t Exponentiate(uint64_t Base, uint64_t Exponent) {
+    uint64_t Exponentiate(const uint64_t Base, const uint64_t Exponent) {
         int64_t Value      = 0;
         int64_t Exponent2  = Exponent;
         if (Base > 0 && Exponent > 0) {
@@ -334,7 +333,7 @@ extern "C" {
         return Value;
     }
     
-    int64_t  Logarithm(int64_t Base, int64_t Exponent) {
+    int64_t  Logarithm(const int64_t Base, const int64_t Exponent) {
         uint64_t Result    = 0ULL;
         int64_t  Exponent2 = Exponent;
         if (Base > 1 && Exponent > 0) {
@@ -351,15 +350,15 @@ extern "C" {
         return Result;
     }
     
-    uint16_t SwapEndian16(uint16_t Integer) {
+    uint16_t SwapEndian16(const uint16_t Integer) {
         return ((Integer & 0xFF00) >> 8) | ((Integer & 0x00FF) << 8);
     }
     
-    uint32_t SwapEndian32(uint32_t Integer) {
+    uint32_t SwapEndian32(const uint32_t Integer) {
         return ((Integer & 0xFF000000) >> 24) | ((Integer & 0x00FF0000) >> 8) | ((Integer & 0x0000FF00) << 8) | ((Integer & 0x000000FF) << 24);
     }
     
-    uint64_t SwapEndian64(uint64_t Integer) {
+    uint64_t SwapEndian64(const uint64_t Integer) {
         return (((Integer & 0xFF00000000000000) >> 56) | ((Integer & 0x00FF000000000000) >> 40) | \
                 ((Integer & 0x0000FF0000000000) >> 24) | ((Integer & 0x000000FF00000000) >>  8) | \
                 ((Integer & 0x00000000FF000000) <<  8) | ((Integer & 0x0000000000FF0000) << 24) | \
@@ -367,11 +366,11 @@ extern "C" {
                 );
     }
     
-    int64_t  Bytes2Bits(int64_t Bytes) {
+    int64_t  Bytes2Bits(const int64_t Bytes) {
         return Bytes * 8;
     }
     
-    int64_t  Bits2Bytes(int64_t Bits, bool RoundUp) {
+    int64_t  Bits2Bytes(const int64_t Bits, const bool RoundUp) {
         uint64_t AbsoluteBits = AbsoluteI(Bits);
         int64_t  Bytes        = 0ULL;
         if (RoundUp == Yes) {
@@ -385,7 +384,7 @@ extern "C" {
         return Bytes;
     }
     
-    uint8_t  SwapBits(uint8_t Byte) {
+    uint8_t  SwapBits(const uint8_t Byte) {
         return (
                 (Byte & 0x80 >> 7) |
                 (Byte & 0x40 >> 5) |
@@ -398,7 +397,7 @@ extern "C" {
                 );
     }
     
-    uint8_t Bits2ExtractFromByte(uint64_t Offset) {
+    uint8_t Bits2ExtractFromByte(const uint64_t Offset) {
         return 8 - (Offset % 8);
     }
     
@@ -421,12 +420,12 @@ extern "C" {
     }
     
     /* Ryū specific math functions */
-    bool IsPowerOfBase(uint8_t Base, uint64_t Value) {
+    bool IsPowerOfBase(const uint8_t Base, const uint64_t Value) {
         return Value % Base == 0 ? Yes : No;
     }
     /* Ryū specific math functions */
     
-    uint8_t GetNumDigitsInBase(uint8_t Base, int64_t Value) {
+    uint8_t GetNumDigitsInBase(const uint8_t Base, const int64_t Value) {
         uint64_t Value2    = AbsoluteI(Value);
         uint8_t  NumDigits = 0;
         do {
@@ -434,12 +433,6 @@ extern "C" {
             NumDigits     += 1;
         } while (Value2 > 0);
         return NumDigits;
-    }
-    
-    uint8_t GetNumBitsToRepresentValue(uint64_t Value) { // This name is so bad
-        return Value;
-        // If the value is a power of 2, shift by 1
-        // Otherwise shift by 1...
     }
     
 #ifdef __cplusplus
