7da446ed611bd6c550f99b129b53298be3ad58e4
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2019-04-13 21:45:39 -0400

    Test_BitIO: New test for writing bits

--- Library/src/Tests/Test_BufferIO.c
+++ Library/src/Tests/Test_BufferIO.c
@@ -11,13 +11,28 @@ extern "C" {
         // Well, we need to test all of the BitIO components, so let's set up a BitInput and BitOutput.
         
         // for testing sockets, we can use the loopback mechanism so only the test runner needs to be available.
-        BitInput  *BitI = BitInput_Init();
-        BitOutput *BitO = BitOutput_Init();
+        
+        typedef enum Constants {
+            WrittenValueSize = 64,
+            WrittenValue     = 1,
+        } Constants;
+        
+        BitInput  *BitI      = BitInput_Init();
+        BitOutput *BitO      = BitOutput_Init();
         
         BitInput_UTF8_OpenFile(BitI, U8("/Users/Marcus/Desktop/TestImages/21.ppm"));
         BitOutput_UTF8_OpenFile(BitO, U8("/Users/Marcus/Desktop/TestImages/21.ppm"));
         
-        uint64_t FileSize = BitInput_GetFileSize(BitI);
+        uint64_t FileSize    = BitInput_GetFileSize(BitI);
+        
+        BitBuffer *BitB      = BitBuffer_Init(8);
+        BitBuffer_WriteBits(LSByteFirst, LSBitFirst, BitB, WrittenValueSize, WrittenValue);
+        uint64_t ReadValue   = BitBuffer_ReadBits(LSByteFirst, LSBitFirst, BitB, WrittenValueSize);
+        
+        if (ReadValue != WrittenValue) {
+            Log(Log_TEST, __func__, U8("Written Value %u does not match the Read Value %llu"), WrittenValue, ReadValue);
+        }
+
         return 0;
     }
     
