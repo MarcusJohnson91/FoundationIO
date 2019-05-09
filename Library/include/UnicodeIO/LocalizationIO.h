bf32e5bc8717ec3494dcaa75cd4b0e301128be44
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2019-05-09 11:25:47 -0400

    LocalizationIO: Added LocalizationIO despite it not being quite done yet, because the fucking thing has been accidentally deleted twice now.

---
+++ Library/include/UnicodeIO/LocalizationIO.h
@@ -0,0 +1,28 @@
+/*!
+ @header              LocalizationIO.h
+ @author              Marcus Johnson
+ @copyright           2019+
+ @version             1.0.0
+ @brief               This header contains types, functions, and tables for cryptography and hashing.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#pragma once
+
+#ifndef FoundationIO_LocalizationIO_H
+#define FoundationIO_LocalizationIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FoundationIO_LocalizationIO_H */
+
