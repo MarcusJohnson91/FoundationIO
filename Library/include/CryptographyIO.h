ad06882a59939faf618c20cce1b4a888949bbbe1
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2019-05-08 23:30:41 -0400

    CryptographyIO: New module for cryptography, primarily for TestIO's random number generation

---
+++ Library/include/CryptographyIO.h
@@ -0,0 +1,27 @@
+/*!
+ @header              CryptographyIO.h
+ @author              Marcus Johnson
+ @copyright           2019+
+ @version             1.0.0
+ @brief               This header contains types, functions, and tables for cryptography and hashing.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#pragma once
+
+#ifndef FoundationIO_CryptographyIO_H
+#define FoundationIO_CryptographyIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    typedef struct MD5 MD5;
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FoundationIO_CryptographyIO_H */
