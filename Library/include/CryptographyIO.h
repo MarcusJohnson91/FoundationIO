b02ad488e1fba4592ef2c1c956d8886acb80c400
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2019-06-13 13:44:29 -0400

    CryptographyIO: MD% stuff is done, mostly

--- Library/include/CryptographyIO.h
+++ Library/include/CryptographyIO.h
@@ -18,7 +18,30 @@
 extern "C" {
 #endif
     
+#ifndef   FoundationIO_StringType8
+#define   FoundationIO_StringType8 (1)
+#ifdef    UTF8
+#undef    UTF8
+#endif /* UTF8 */
+#if (defined __STDC_UTF_8__ && defined __CHAR8_TYPE__ && __STDC_VERSION__ >= FoundationIOSTDVersionC2X) && (FoundationIOTargetOS != FoundationIOAppleOS)
+    typedef   char8_t        UTF8;
+#else
+    typedef   unsigned char  UTF8;
+#endif /* __CHAR8_TYPE__ */
+#endif /* FoundationIO_StringType8 */
+    
     typedef struct MD5 MD5;
+    
+    MD5  *MD5_Init(void);
+    
+    void  MD5_Process(uint32_t *State, uint32_t *Block);
+    
+    UTF8 *MD5_Finalize(MD5 *MD5);
+    
+    bool  MD5_Compare(uint8_t *Hash1, uint8_t *Hash2);
+    
+    void  MD5_Deinit(MD5 *MD5);
+    
     
 #ifdef __cplusplus
 }
