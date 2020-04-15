2ace3a0fd46064d815ec36de3bd38a09896eec45
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2020-04-14 20:51:34 -0400

    Prettified LocalizationIO header

--- Library/include/UnicodeIO/LocalizationIO.h
+++ Library/include/UnicodeIO/LocalizationIO.h
@@ -6,16 +6,15 @@
  @brief               This header contains types, functions, and tables for cryptography and hashing.
  */
 
-#include "../Macros.h"
-#include "../Constants.h"
-#include "UnicodeTypes.h"
+#include  "UnicodeTypes.h"
+#include  "../Constants.h"
 
 #pragma once
 
 #ifndef FoundationIO_UnicodeIO_LocalizationIO_H
 #define FoundationIO_UnicodeIO_LocalizationIO_H
 
-#ifdef __cplusplus
+#if (FoundationIOLanguage == FoundationIOLanguageIsCXX)
 extern "C" {
 #endif
     
@@ -61,7 +60,7 @@ extern "C" {
     
     /*!
      @enum       LocalizationIO_SpokenLanguages
-     @abstract   What language is in use, for colation, sorting, etc.
+     @abstract                                 What language is in use, for colation, sorting, etc.
      @constant   SpokenLanguage_Unspecified    Unspecified, falls back to locale.
      */
     typedef enum LocalizationIO_SpokenLanguages {
@@ -97,180 +96,180 @@ extern "C" {
     } LocalizationIO_SpokenLanguages;
     
     /*!
-     @enum                      LocalizationIO_RegionIDs
-     @abstract                                                  "What region is in use, for collation, sorting, etc".
-     @constant                  LanguageID_Unknown              "Unknown file type".
-     @constant                  LanguageID_Default              "Equilivent to C locale".
-     @constant                  LanguageID_English              "The language is English".
-     @constant                  LanguageID_Swedish              "The language is Swedish".
-     @constant                  LanguageID_Norwegian            "The language is Norwegian".
-     @constant                  LanguageID_Icelandic            "The language is Icelandic".
-     @constant                  LanguageID_German               "The language is German".
-     @constant                  LanguageID_Welsh                "The language is Welsh".
-     @constant                  LanguageID_French               "The language is French".
-     @constant                  LanguageID_Lithuanian           "The language is Lithuanian".
-     @constant                  LanguageID_Russian              "The language is Russian".
-     @constant                  LanguageID_Polish               "The language is Polish".
-     @constant                  LanguageID_Danish               "The language is Danish".
-     @constant                  LanguageID_Scottish             "The language is Scottish".
-     @constant                  LanguageID_Irish                "The language is Irish".
-     @constant                  LanguageID_Afrikaans            "The language is Afrikaans".
-     @constant                  LanguageID_Faroese              "The language is Faroese".
-     @constant                  LanguageID_Croatian             "The language is Croatian".
+     @enum       LocalizationIO_RegionIDs
+     @abstract                                    What region is in use, for collation, sorting, etc.
+     @constant   LanguageID_Unknown               Unknown file type.
+     @constant   LanguageID_Default               Equilivent to C locale.
+     @constant   LanguageID_English               The language is English.
+     @constant   LanguageID_Swedish               The language is Swedish.
+     @constant   LanguageID_Norwegian             The language is Norwegian.
+     @constant   LanguageID_Icelandic             The language is Icelandic.
+     @constant   LanguageID_German                The language is German.
+     @constant   LanguageID_Welsh                 The language is Welsh.
+     @constant   LanguageID_French                The language is French.
+     @constant   LanguageID_Lithuanian            The language is Lithuanian.
+     @constant   LanguageID_Russian               The language is Russian.
+     @constant   LanguageID_Polish                The language is Polish.
+     @constant   LanguageID_Danish                The language is Danish.
+     @constant   LanguageID_Scottish              The language is Scottish.
+     @constant   LanguageID_Irish                 The language is Irish.
+     @constant   LanguageID_Afrikaans             The language is Afrikaans.
+     @constant   LanguageID_Faroese               The language is Faroese.
+     @constant   LanguageID_Croatian              The language is Croatian.
      */
     typedef enum LocalizationIO_RegionIDs {
-                                RegionID_Unknown            = 0,
-                                RegionID_Default            = 1, // Equilivent to "C" locale
-                                RegionID_UnitedStates       = 2,
-                                RegionID_Canada             = 3,
-                                RegionID_Mexico             = 4,
-                                RegionID_UnitedKingdom      = 5,
-                                RegionID_Sweden             = 6,
-                                RegionID_Denmark            = 7,
-                                RegionID_Norway             = 8,
-                                RegionID_Finland            = 9,
-                                RegionID_Estonia            = 10,
-                                RegionID_Latvia             = 11,
-                                RegionID_Lithuania          = 12,
-                                RegionID_Poland             = 13,
-                                RegionID_Russia             = 14,
-                                RegionID_Croatia            = 15,
+                 RegionID_Unspecified        = 0,
+                 RegionID_UnitedStates       = 1,
+                 RegionID_Canada             = 2,
+                 RegionID_Mexico             = 3,
+                 RegionID_UnitedKingdom      = 4,
+                 RegionID_Sweden             = 5,
+                 RegionID_Denmark            = 6,
+                 RegionID_Norway             = 7,
+                 RegionID_Finland            = 8,
+                 RegionID_Estonia            = 9,
+                 RegionID_Latvia             = 10,
+                 RegionID_Lithuania          = 11,
+                 RegionID_Poland             = 12,
+                 RegionID_Russia             = 13,
+                 RegionID_Croatia            = 14,
     } LocalizationIO_RegionIDs;
     
-    typedef enum LocalizationIO_EncodingIDs {
-                                EncodingID_Unknown          = 0,
-                                EncodingID_UTF8             = 1,
-                                EncodingID_UTF16            = 2,
-                                EncodingID_UTF32            = 3,
-    } LocalizationIO_EncodingIDs;
-    
+    /*!
+     @enum       LocalizationIO_DecimalSeperators
+     @abstract                                    What decimal seperator is in use, for collation, sorting, etc.
+     */
     typedef enum LocalizationIO_DecimalSeperators {
-                                DecimalSeperator_Unknown    = 0,
-                                DecimalSeperator_Period     = 1,
-                                DecimalSeperator_Comma      = 2,
-                                DecimalSeperator_Apostrophe = 3,
+                 DecimalSeperator_Unknown    = 0,
+                 DecimalSeperator_Period     = 1,
+                 DecimalSeperator_Comma      = 2,
+                 DecimalSeperator_Apostrophe = 3,
     } LocalizationIO_DecimalSeperators;
     
+    /*!
+     @enum       LocalizationIO_GroupSeperators
+     @abstract                                    What group seperator is in use, for collation, sorting, etc.
+     */
     typedef enum LocalizationIO_GroupSeperators {
-                                GroupSeperator_Unknown      = 0,
-                                GroupSeperator_Comma        = 1,
-                                GroupSeperator_Period       = 2,
-                                GroupSeperator_Underscore   = 3,
-                                GroupSeperator_Space        = 4,
+                 GroupSeperator_Unknown      = 0,
+                 GroupSeperator_Comma        = 1,
+                 GroupSeperator_Period       = 2,
+                 GroupSeperator_Underscore   = 3,
+                 GroupSeperator_Space        = 4,
     } LocalizationIO_GroupSeperators;
     
     /*!
-     @abstract                                              "Creates a string containing the language code (en for englisn, etc)".
-     @return                                                "Returns the LanguageID".
+     @abstract                                               Creates a string containing the language code (en for englisn, etc).
+     @return                                                 Returns the LanguageID.
      */
-     LocalizationIO_WrittenLanguages Localize_GetWrittenLanguageID(void);
+    LocalizationIO_WrittenLanguages Localize_GetWrittenLanguageID(void);
     
     /*!
-     @abstract                                              "Creates a string containing the region code (us for America, etc)".
-     @return                                                "Returns the RegionID".
+     @abstract                                               Creates a string containing the region code (us for America, etc).
+     @return                                                 Returns the RegionID.
      */
     LocalizationIO_RegionIDs    Localize_GetRegionID(void);
     
     /*!
-     @abstract                                              "Gets the encoding".
-     @return                                                "Returns the EncodingID".
+     @abstract                                              Gets the encoding.
+     @return                                                 Returns the EncodingID.
      */
-    FoundationIO_StringTypes  Localize_GetEncodingID(void);
+    FoundationIO_StringTypes     Localize_GetEncodingID(void);
     
     /*!
-     @abstract                                              "Creates a string containing the symbol to seperate the main value from the decimal (XXX.YYY)".
-     @return                                                "Returns the string".
+     @abstract                                               Creates a string containing the symbol to seperate the main value from the decimal (XXX.YYY).
+     @return                                                 Returns the string.
      */
     UTF8                       *Localize_UTF8_GetDecimalSeperator(void);
     
     /*!
-     @abstract                                              "Creates a string containing the symbol to seperate the main value from the decimal (XXX.YYY)".
-     @return                                                "Returns the string".
+     @abstract                                               Creates a string containing the symbol to seperate the main value from the decimal (XXX.YYY).
+     @return                                                 Returns the string.
      */
     UTF16                      *Localize_UTF16_GetDecimalSeperator(void);
     
     /*!
-     @abstract                                              "Creates a string containing the symbol to seperate groups (1,000,000)".
-     @return                                                "Returns the string".
+     @abstract                                               Creates a string containing the symbol to seperate groups (1,000,000).
+     @return                                                 Returns the string.
      */
     UTF8                       *Localize_UTF8_GetGroupingSeperator(void);
     
     /*!
-     @abstract                                              "Creates a string containing the symbol to seperate groups (1,000,000)".
-     @return                                                "Returns the string".
+     @abstract                                               Creates a string containing the symbol to seperate groups (1,000,000).
+     @return                                                 Returns the string.
      */
     UTF16                      *Localize_UTF16_GetGroupingSeperator(void);
     
     /*!
-     @abstract                                              "Creates a string array containing the number of digits for each group (1,000,000)".
-     @return                                                "Returns the string".
+     @abstract                                               Creates a string array containing the number of digits for each group (1,000,000).
+     @return                                                 Returns the string.
      */
     UTF8                      **Localize_UTF8_GetGroupingSize(void);
     
     /*!
-     @abstract                                              "Creates a string array containing the number of digits for each group (1,000,000)".
-     @return                                                "Returns the string".
+     @abstract                                               Creates a string array containing the number of digits for each group (1,000,000).
+     @return                                                 Returns the string.
      */
     UTF16                     **Localize_UTF16_GetGroupingSize(void);
     
     /*!
-     @abstract                                              "Creates a string containing the currency symbol ($1)".
-     @return                                                "Returns the string".
+     @abstract                                               Creates a string containing the currency symbol ($1).
+     @return                                                 Returns the string.
      */
     UTF8                       *Localize_UTF8_GetCurrencySymbol(void);
     
     /*!
-     @abstract                                              "Creates a string containing the currency symbol ($1)".
-     @return                                                "Returns the string".
+     @abstract                                               Creates a string containing the currency symbol ($1).
+     @return                                                 Returns the string.
      */
     UTF16                      *Localize_UTF16_GetCurrencySymbol(void);
     
     /*!
-     @abstract                                              "Creates a string containing just the base 10 digits from String".
-     @param                     Base                        "What base is the integer in"?
-     @param                     String                      "The string to Delocalize".
-     @return                                                "Returns the delocalized string".
+     @abstract                                               Creates a string containing just the base 10 digits from String.
+     @param                     Base                         What base is the integer in?
+     @param                     String                       The string to Delocalize.
+     @return                                                 Returns the delocalized string.
      */
     UTF8                       *UTF8_DelocalizeInteger(FoundationIO_Bases Base, UTF8 *String);
     
     /*!
-     @abstract                                              "Creates a string containing just the base 10 digits from String".
-     @param                     Base                        "What base is the integer in"?
-     @param                     String                      "The string to Delocalize".
-     @return                                                "Returns the delocalized string".
+     @abstract                                               Creates a string containing just the base 10 digits from String.
+     @param                     Base                         What base is the integer in?
+     @param                     String                       The string to Delocalize.
+     @return                                                 Returns the delocalized string.
      */
     UTF16                      *UTF16_DelocalizeInteger(FoundationIO_Bases Base, UTF16 *String);
     
     /*!
-     @abstract                                              "Creates a string containing just the base 10 digits from String".
-     @param                     Base                        "What base is the integer in"?
-     @param                     String                      "The string to Delocalize".
-     @return                                                "Returns the delocalized string".
+     @abstract                                               Creates a string containing just the base 10 digits from String.
+     @param                     Base                         What base is the integer in?
+     @param                     String                       The string to Delocalize.
+     @return                                                 Returns the delocalized string.
      */
     UTF32                      *UTF32_DelocalizeInteger(FoundationIO_Bases Base, UTF32 *String);
     
     /*!
-     @abstract                                              "Creates a string containing just the base 10 digits from String".
-     @param                     String                      "The string to Delocalize".
-     @return                                                "Returns the delocalized string".
+     @abstract                                               Creates a string containing just the base 10 digits from String.
+     @param                     String                       The string to Delocalize.
+     @return                                                 Returns the delocalized string.
      */
     UTF8                       *UTF8_DelocalizeDecimal(UTF8 *String);
     
     /*!
-     @abstract                                              "Creates a string containing just the base 10 digits from String".
-     @param                     String                      "The string to Delocalize".
-     @return                                                "Returns the delocalized string".
+     @abstract                                               Creates a string containing just the base 10 digits from String.
+     @param                     String                       The string to Delocalize.
+     @return                                                 Returns the delocalized string.
      */
     UTF16                      *UTF16_DelocalizeDecimal(UTF16 *String);
     
     /*!
-     @abstract                                              "Creates a string containing just the base 10 digits from String".
-     @param                     String                      "The string to Delocalize".
-     @return                                                "Returns the delocalized string".
+     @abstract                                               Creates a string containing just the base 10 digits from String.
+     @param                     String                       The string to Delocalize.
+     @return                                                 Returns the delocalized string.
      */
     UTF32                      *UTF32_DelocalizeDecimal(UTF32 *String);
     
-#ifdef __cplusplus
+#if (FoundationIOLanguage == FoundationIOLanguageIsCXX)
 }
 #endif
 
