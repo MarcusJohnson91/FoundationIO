6c173b7a47f56619e273c14824bca1d713d9c974
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2020-07-18 09:43:21 -0400

    Constants.h: All constants except BitMaskTable moved to UnicodeTables.h

--- Library/include/Private/Constants.h
+++ Library/include/Private/Constants.h
@@ -20,68 +20,8 @@ extern "C" {
 
 #define BitMaskTableSize             8
 
-#define IntegerTableBase2Size        2
-
-#define IntegerTableBase8Size        8
-
-#define TableBase10Size             10
-
-#define IntegerTableBase16Size      16
-
-#define DecimalTableBase10Size      11
-
-#define DecimalTableScientificSize  14
-
-#define DecimalTableHexadecimalSize 21
-
-#define MathSeperatorTableSize       4
-
 	static const uint8_t BitMaskTable[BitMaskTableSize] = {
 		0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF
-	};
-
-	static const UTF32 IntegerTableBase2[IntegerTableBase2Size] = {
-		U'0', U'1'
-	};
-
-	static const UTF32 IntegerTableBase8[IntegerTableBase8Size] = {
-		U'0', U'1', U'2', U'3', U'4', U'5', U'6', U'7'
-	};
-
-	static const UTF32 TableBase10[TableBase10Size] = {
-		U'0', U'1', U'2', U'3', U'4', U'5', U'6', U'7', U'8', U'9'
-	};
-
-	static const UTF32 IntegerTableUppercaseBase16[IntegerTableBase16Size] = {
-		U'0', U'1', U'2', U'3', U'4', U'5', U'6', U'7', U'8', U'9', U'A', U'B',
-		U'C', U'D', U'E', U'F'
-	};
-
-	static const UTF32 IntegerTableLowercaseBase16[IntegerTableBase16Size] = {
-		U'0', U'1', U'2', U'3', U'4', U'5', U'6', U'7', U'8', U'9', U'a', U'b',
-		U'c', U'd', U'e', U'f'
-	};
-
-	static const UTF32 DecimalScientificUppercase[DecimalTableScientificSize] = {
-		U'0', U'1', U'2', U'3', U'4', U'5', U'6', U'7', U'8', U'9', U'E'
-	};
-
-	static const UTF32 DecimalScientificLowercase[DecimalTableScientificSize] = {
-		U'0', U'1', U'2', U'3', U'4', U'5', U'6', U'7', U'8', U'9', U'e'
-	};
-
-	static const UTF32 DecimalHexUppercase[DecimalTableHexadecimalSize] = {
-		U'0', U'1', U'2', U'3', U'4', U'5', U'6', U'7', U'8', U'9', U'A',
-		U'B', U'C', U'D', U'E', U'F', U'P', U'X'
-	};
-
-	static const UTF32 DecimalHexLowercase[DecimalTableHexadecimalSize] = {
-		U'0', U'1', U'2', U'3', U'4', U'5', U'6', U'7', U'8', U'9', U'a',
-		U'b', U'c', U'd', U'e', U'f', U'p', U'x'
-	};
-
-	static const UTF32 MathSeperators[MathSeperatorTableSize] = {
-		U',', U'.', U'\'', U' '
 	};
 
 #if (PlatformIO_Language == PlatformIO_LanguageIsCXX)
