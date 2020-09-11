b9a94c977154708153cb2e72ebce1b65518ae03c
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2020-09-11 13:13:04 -0700

    Cleanup

--- Library/src/Tests/Test_FileIO.c
+++ Library/src/Tests/Test_FileIO.c
@@ -6,14 +6,12 @@
 extern "C" {
 #endif
 
-    // Test BOM
-
-    bool Test_FileOpenWriteReadClose(SecureRNG *Secure) {
+    bool Test_FileOpenWriteReadClose8(SecureRNG *Secure) {
         bool TestPassed = No;
-        FILE *Write     = FileIO_OpenUTF8(UTF8String("/Users/Marcus/Desktop/FileIO_Test.bin"), FileMode_Write | FileMode_Binary);
+        FILE *Write     = FileIO_OpenUTF8(UTF8String("\xEF\xBB\xBF/Users/Marcus/Desktop/FileIO_Test.bin"), FileMode_Write | FileMode_Binary);
         FileIO_Write(Write, UTF8String("Wat"), 1, 4);
         UTF8  String[4];
-        FILE *Read      = FileIO_OpenUTF8(UTF8String("/Users/Marcus/Desktop/FileIO_Test.bin"), FileMode_Read | FileMode_Binary);
+        FILE *Read      = FileIO_OpenUTF8(UTF8String("\xEF\xBB\xBF/Users/Marcus/Desktop/FileIO_Test.bin"), FileMode_Read | FileMode_Binary);
         FileIO_Read(Read, &String, 1, 3);
         if (String[0] == 'W' && String[1] == 'a' && String[2] == 't') {
             TestPassed  = Yes;
@@ -21,13 +19,19 @@ extern "C" {
         FileIO_Seek(Read, -1, SeekType_Current);
         UTF8 Seeked = 0;
         FileIO_Read(Read, &Seeked, 1, 1);
-
+        if (Seeked == 't') {
+            TestPassed = Yes;
+        }
         return TestPassed;
     }
 
     int main(const int argc, const char *argv[]) {
-        bool   TestSuitePassed      = Test_FileOpenWriteReadClose(NULL);
-        return TestSuitePassed;
+        bool   TestSuitePassed      = Test_FileOpenWriteReadClose8(NULL);
+        int    ExitCode             = EXIT_FAILURE;
+        if (TestSuitePassed) {
+            ExitCode                = EXIT_SUCCESS;
+        }
+        return ExitCode;
     }
 
 #if (PlatformIO_Language == PlatformIO_LanguageIsCXX)
