977b6e25baae41c8f30c1eb740aedbed7b3d2f99
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2020-09-22 07:56:33 -0700

    Xcode, Cmake, and Resources

--- Library/include/Private/FoundationIO_Version.h
+++ Library/include/Private/FoundationIO_Version.h
@@ -2,19 +2,12 @@
  @header          FoundationIO_Version.h
  @author          Marcus Johnson
  @copyright       2020+
- @version         1.0.0
+ @version         0.9.7
  @brief           This header contains macros that define FoundationIO's version.
  */
 
-#include "../PlatformIO.h" /* Included for Platform Independence macros */
-
 #pragma once
-
-#ifndef FoundationIO_Version_H
-#define FoundationIO_Version_H
 
 #define FoundationIO_Version_Major 0
 #define FoundationIO_Version_Minor 9
 #define FoundationIO_Version_Patch 7
-
-#endif /* FoundationIO_Version_H */
