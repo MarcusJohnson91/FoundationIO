cb67ed641ff32d95bf1d2ae0d768f314051e6e39
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2020-10-17 08:17:17 -0700

    TextIO_Bases straggler

--- Tests/TextIO/Test_LocalizationIO.c
+++ Tests/TextIO/Test_LocalizationIO.c
@@ -10,7 +10,7 @@ extern "C" {
         bool TestPassed                         = Yes;
         PlatformIO_Immutable(UTF8*) Unlocalized = UTF8String("1000000000");
         PlatformIO_Immutable(UTF8*) Localized   = UTF8String("1,000,000,000");
-        UTF8                     *Delocalized   = UTF8_DelocalizeInteger(Localized, Base_Integer | Base_Radix10);
+        UTF8                     *Delocalized   = UTF8_DelocalizeInteger(Base_Integer | Base_Radix10, Localized);
         if (UTF8_Compare(Delocalized, Unlocalized) == false) {
             TestPassed                          = No;
             Log(Severity_DEBUG, PlatformIO_FunctionName, UTF8String("Delocalize failed: %s does not match %s"), Delocalized, Unlocalized);
@@ -22,20 +22,20 @@ extern "C" {
         bool TestPassed                         = Yes;
         PlatformIO_Immutable(UTF8*) Unlocalized = UTF8String("1000000000.0");
         PlatformIO_Immutable(UTF8*) Localized   = UTF8String("1,000,000,000.0");
-        UTF8                     *Delocalized   = UTF8_DelocalizeInteger(Localized, Base_Integer | Base_Radix10);
+        UTF8                     *Delocalized   = UTF8_DelocalizeInteger(Base_Integer | Base_Radix10, Localized);
         if (UTF8_Compare(Delocalized, Unlocalized) == false) {
             TestPassed                          = No;
             Log(Severity_DEBUG, PlatformIO_FunctionName, UTF8String("Delocalize failed: %s does not match %s"), Delocalized, Unlocalized);
         }
         return TestPassed;
     }
-    
+
     int main(const int argc, const char *argv[]) {
         bool TestsPassed = No;
         TestsPassed      = Test_DelocalizeInteger();
         return TestsPassed;
     }
-    
+
 #if (PlatformIO_Language == PlatformIO_LanguageIsCXX)
 }
 #endif
