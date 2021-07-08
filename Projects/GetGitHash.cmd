1aea728c257bba2b594411406a8f95a442cc0e85
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2021-07-07 19:01:46 -0700

    Windows resource file work

--- Projects/GetGitHash.cmd
+++ Projects/GetGitHash.cmd
@@ -2,19 +2,20 @@
 SETLOCAL EnableExtensions
 SETLOCAL EnableDelayedExpansion
 
-echo|set /p="#define Version_CommitID " !CommitID_U! >"%~dp0\..\Library\include\Private\CommitID.h"
+SET CommitID_Header="%~dp0\..\Library\include\Private\CommitID.h"
 
-FOR /F %%A IN ('git branch --show-current') DO SET GitBranch=%%A
+FOR /F %%A IN ('git branch --show-current') DO ( SET GitBranch=%%A )
 
-FOR /F %%A IN ('git rev-parse !GitBranch!') DO SET CommitID=%%A
+FOR /F %%A IN ('git rev-parse !GitBranch!') DO ( SET CommitID=%%A )
 
-SET CommitID_U=!CommitID!
-CALL :HexUpper CommitID_U
-echo !CommitID_U!>>"%~dp0\..\Library\include\Private\CommitID.h"
-exit /b
+CALL :HexUpper CommitID
+ECHO #define Version_CommitID !CommitID! >!CommitID_Header!
+ECHO #define Version_CommitIDMin !CommitID:~36,4! >>!CommitID_Header!
+
+GOTO :EOF
 
 :HexUpper
-FOR %%i IN ("0=0" "1=1" "2=2" "3=3" "4=4" "5=5" "6=6" "7=7" "8=8" "9=9" "a=A" "b=B" "c=C" "d=D" "e=E" "f=F") DO CALL SET "%1=%%%1:%%~i%%"
-exit /b
+FOR %%i IN ("0=0" "1=1" "2=2" "3=3" "4=4" "5=5" "6=6" "7=7" "8=8" "9=9" "a=A" "b=B" "c=C" "d=D" "e=E" "f=F") DO ( CALL SET "%1=%%%1:%%~i%%" )
+EXIT /b
 
 ENDLOCAL
