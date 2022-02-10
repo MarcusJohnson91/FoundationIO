a4db20e08d9d7ccdf0781b890c1e3255bae13e3f
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2022-02-10 05:20:07 -0500

    Xcode project from scratch

---
+++ Library/FoundationIO.h
@@ -0,0 +1,32 @@
+/*!
+ @header          FoundationIO.h
+ @author          Marcus Johnson
+ @copyright       2015+
+ @version         0.9.7
+ @brief           This header contains all of FoundationIO's public API in one easy to include header.
+ */
+
+#pragma once
+
+#ifndef FoundationIO_h
+#define FoundationIO_h
+
+#include "include/AssertIO.h"
+#include "include/AsynchronousIO.h"
+#include "include/BufferIO.h"
+#include "include/CryptographyIO.h"
+#include "include/FileIO.h"
+#include "include/GUUID.h"
+#include "include/MathIO.h"
+#include "include/NetworkIO.h"
+#include "include/PlatformIO.h"
+#include "include/TestIO.h"
+#include "include/TextIO/TextIOTypes.h"
+#include "include/TextIO/CommandLineIO.h"
+#include "include/TextIO/FormatIO.h"
+#include "include/TextIO/LocalizationIO.h"
+#include "include/TextIO/LogIO.h"
+#include "include/TextIO/StringIO.h"
+#include "include/TextIO/StringSetIO.h"
+
+#endif /* FoundationIO_h */
