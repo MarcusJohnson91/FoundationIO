c6322a32a08910e1a2d8cfe46d14172fd9e13f8e
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2022-02-11 13:42:09 -0500

    Cmake

---
+++ Projects/BuildSettings.cmake
@@ -0,0 +1,290 @@
+if(CMAKE_HOST_UNIX)
+    add_definitions(-D_LARGEFILE_SOURCE)
+    add_definitions(-D_LARGEFILE64_SOURCE)
+    add_definitions(-D_FILE_OFFSET_BITS=64)
+    add_definitions(-D__STDC_WANT_LIB_EXT1__)
+    add_definitions(-D__TIMESIZE=64)
+elseif(CMAKE_HOST_WIN32)
+    add_definitions(-DUNICODE)
+    add_definitions(-D_UNICODE)
+    add_definitions(-D_ATL_ALLOW_CHAR_UNSIGNED)
+    add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+endif(CMAKE_HOST_UNIX)
+
+if($<C_COMPILER_ID:AppleClang,ARMClang,XLClang,Clang>)
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fsanitize=address,undefined -fno-omit-frame-pointer")
+
+set(CMAKE_C_FLAGS " \
+${CMAKE_C_FLAGS} \
+-std=c2x \
+-fPIC \
+-Wall \
+-Wextra \
+-Weverything \
+-Bsymbolic \
+-Bsymbolic-functions \
+-ferror-limit=1024 \
+-foptimize-sibling-calls \
+-fshort-enums \
+-funsigned-char \
+-fvisibility-inlines-hidden \
+-Wenum-conversion \
+-Wno-assign-enum \
+-Wno-c99-compat \
+-Wno-ignored-attributes \
+-Wno-incompatible-pointer-types-discards-qualifiers \
+-Wno-padded \
+-Wno-pointer-sign \
+-Wno-reserved-id-macro \
+-Wno-sign-conversion \
+-Wpedantic \
+-Wreserved-identifier \
+-Wswitch-enum \
+")
+
+set(CMAKE_C_FLAGS_DEBUG " \
+${CMAKE_C_FLAGS} \
+-fsanitize=address,undefined \
+-fno-omit-frame-pointer \
+-fno-optimize-sibling-calls \
+-fstack-check \
+-DDEBUG=1 \
+-g3 \
+-O1 \
+-glldb \
+")
+
+set(CMAKE_C_FLAGS_RELEASE " \
+${CMAKE_C_FLAGS} \
+-Ofast \
+")
+
+set(CMAKE_C_FLAGS_RELWITHDEBINFO " \
+${CMAKE_C_FLAGS} \
+-fsanitize=address,undefined \
+-fno-omit-frame-pointer \
+-fno-optimize-sibling-calls \
+-fstack-check \
+-DDEBUG=1 \
+-g \
+-Ofast \
+")
+
+set(CMAKE_C_FLAGS_MINSIZEREL " \
+${CMAKE_C_FLAGS} \
+-Oz \
+")
+
+set(LINK_FLAGS ${LINK_FLAGS} " \
+-flto=full \
+-lm \
+")
+
+if(CMAKE_HOST_APPLE)
+set(LINK_FLAGS ${LINK_FLAGS} " \
+-dead_strip_dylibs
+")
+endif(CMAKE_HOST_APPLE)
+
+set(LINK_FLAGS_DEBUG ${LINK_FLAGS} " \
+-fsanitize=address,undefined \
+-fno-omit-frame-pointer \
+")
+
+set(LINK_FLAGS_RELWITHDEBINFO ${LINK_FLAGS} " \
+-fsanitize=address,undefined \
+-fno-omit-frame-pointer \
+")
+
+set(LINK_FLAGS_RELEASE ${LINK_FLAGS} " \
+")
+
+set(LINK_FLAGS_MINSIZEREL ${LINK_FLAGS} " \
+")
+
+elseif($<C_COMPILER_ID:GNU>)
+
+set(CMAKE_C_FLAGS " \
+${CMAKE_C_FLAGS} \
+-fPIC \
+-fshort-enums \
+-funroll-loops \
+-funsigned-char \
+-std=c2x \
+-Wall \
+-Wextra \
+-Wpedantic \
+-Wno-ignored-attributes \
+-Wno-padded \
+-Wno-pointer-sign \
+-Wno-sign-conversion \
+-Wno-varargs \
+-Wsuggest-attribute=const \
+-Wsuggest-attribute=pure \
+-Wsuggest-attribute=malloc \
+-Wsuggest-attribute=noreturn \
+-Wsuggest-attribute=format \
+-Wmissing-format-attribute \
+-Wmissing-noreturn \
+-Wswitch-enum \
+-Wreserved-identifier \
+-Bsymbolic \
+-fno-semantic-interposition \
+-fvisibility-inlines-hidden \
+-foptimize-sibling-calls \
+")
+
+set(CMAKE_C_FLAGS_DEBUG " \
+${CMAKE_C_FLAGS} \
+-fno-omit-frame-pointer \
+-fsanitize=address,undefined \
+-fstack-check \
+-DDEBUG=1 \
+-g \
+-O1 \
+")
+
+set(CMAKE_C_FLAGS_RELEASE " \
+${CMAKE_C_FLAGS} \
+-Ofast \
+")
+
+set(CMAKE_C_FLAGS_RELWITHDEBINFO " \
+${CMAKE_C_FLAGS} \
+-fno-omit-frame-pointer \
+-fsanitize=address,undefined \
+-fno-omit-frame-pointer \
+-fstack-check \
+-DDEBUG=1 \
+-g \
+-Ofast \
+")
+
+set(CMAKE_C_FLAGS_MINSIZEREL " \
+${CMAKE_C_FLAGS} \
+-Os \
+")
+
+set(LINK_FLAGS ${LINK_FLAGS} " \
+-flto=full \
+-lm \
+")
+
+if(CMAKE_HOST_APPLE)
+set(LINK_FLAGS ${LINK_FLAGS} " \
+-dead_strip_dylibs
+")
+endif(CMAKE_HOST_APPLE)
+
+set(LINK_FLAGS_DEBUG " \
+${LINK_FLAGS} \
+-fno-omit-frame-pointer \
+-fsanitize=address,undefined \
+")
+
+set(LINK_FLAGS_RELWITHDEBINFO " \
+${LINK_FLAGS} \
+-fno-omit-frame-pointer \
+-fsanitize=address,undefined \
+")
+
+set(LINK_FLAGS_RELEASE " \
+${LINK_FLAGS} \
+")
+
+set(LINK_FLAGS_MINSIZEREL " \
+${LINK_FLAGS} \
+")
+
+elseif($<C_COMPILER_ID:MSVC>)
+
+set(CMAKE_C_FLAGS " \
+${CMAKE_C_FLAGS} \
+/bigobj \
+/FAu \
+/GF \
+/Gm- \
+/GT \
+/Gy \
+/JMC \
+/J \
+/MP \
+/MT \
+/Ob2 \
+/openmp \
+/p:CharacterSet=Unicode \
+/p:FunctionLevelLinking=true \
+/p:PrecompiledHeader=NotUsing \
+/p:RuntimeLibrary=MultiThreaded \
+/p:UseOfMfc=false \
+/p:WarningLevel=Level3 \
+/Qpar \
+/sdl- \
+/std:c++17 \
+/W3 \
+/Zc:rvalueCast \
+/Zc:wchar_t \
+/Wall \
+")
+
+set(CMAKE_C_FLAGS_DEBUG " \
+${CMAKE_C_FLAGS} \
+/DDEBUG=1 \
+/D_DEBUG=1 \
+/p:UseDebugLibraries=true \
+/O1 \
+")
+
+set(CMAKE_C_FLAGS_RELEASE " \
+${CMAKE_C_FLAGS} \
+/p:UseDebugLibraries=false \
+/Ox \
+")
+
+set(CMAKE_C_FLAGS_RELWITHDEBINFO " \
+${CMAKE_C_FLAGS} \
+/DDEBUG=1 \
+/D_DEBUG=1 \
+/p:UseDebugLibraries=true \
+-Ox \
+")
+
+set(CMAKE_C_FLAGS_MINSIZEREL " \
+${CMAKE_C_FLAGS} \
+/p:UseDebugLibraries=false \
+-Os \
+")
+
+set(LINK_FLAGS " \
+${LINK_FLAGS} \
+/GL \
+/LTCG:INCREMENTAL
+/OPT:REF
+/flto \
+")
+
+set(LINK_FLAGS_DEBUG " \
+${LINK_FLAGS} \
+/DEBUG \
+")
+
+
+set(LINK_FLAGS_RELWITHDEBINFO " \
+${LINK_FLAGS} \
+/DEBUG \
+/RELEASE \
+")
+
+set(LINK_FLAGS_RELEASE " \
+${LINK_FLAGS} \
+/RELEASE \
+")
+
+set(LINK_FLAGS_MINSIZEREL " \
+${LINK_FLAGS} \
+/RELEASE \
+")
+
+endif($<C_COMPILER_ID:AppleClang,ARMClang,XLClang,Clang>)
+
--- Projects/GetVersion.cmake
+++ Projects/GetVersion.cmake
@@ -4,39 +4,34 @@ cmake_policy(SET CMP0066 NEW)
 find_package(Git)
 
 if(GIT_FOUND)
-function(GetVersionString VERSION_DIRECTORY VERSION_STRING)
-    get_filename_component(PARENT_DIR ${VERSION_DIRECTORY} DIRECTORY)
+function(GetVersionString VERSION_FILE OUTPUT_VERSION_STRING)
+    file(READ ${VERSION_FILE} Version_Header)
+    set(VERSION_LINE "")
+    string(REGEX MATCH " @version         ([0-9]+)\.([0-9]+)\.([0-9]+)$" VERSION_LINE ${Version_Header})
+    set("Version_Major" ${CMAKE_MATCH_2})
+    set("Version_Minor" ${CMAKE_MATCH_3})
+    set("Version_Patch" ${CMAKE_MATCH_4})
 
-    string(CONCAT VersionHeaderPath "${PARENT_DIR}/Version.h")
-    file(READ ${VersionHeaderPath} Version_Header)
+    # cd /path/to/parent/repo && git ls-files -s yourSubmodule
+    # How do we detect if we're a submodule though?
+    # FoundationIO is always Dependencies/FoundationIO; LIVC is Library/External/LIVC though
+    # Go up one folder and check if it's Dependencies or External?
+    # Could just create a variable in the owning repo
 
-    string(CONCAT CommitIDHeaderPath "${PARENT_DIR}/CommitID.h")
+    # like FoundationIO's cmakelist would say it's the original, and OVIA could set it that it's a submodule
 
-    if(CMAKE_HOST_UNIX)
-        execute_process(COMMAND sh "${BASE_DIRECTORY}/Projects/GetGitHash.sh")
-    else(CMAKE_HOST_WIN32)
-        execute_process(COMMAND cmd.exe "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/Scripts/GetGitHash.cmd")
-    endif(CMAKE_HOST_UNIX)
+    set(VERSION_PATH "")
 
-    file(READ ${CommitIDHeaderPath} CommitID_Header)
-    
-    string(REGEX MATCH "#define Version_Major ([0-9]+)" MATCH_STRING ${Version_Header})
-    set("VERSION_MAJOR" ${CMAKE_MATCH_1})
-    string(REGEX MATCH "#define Version_Minor ([0-9]+)" MATCH_STRING ${Version_Header})
-    set("VERSION_MINOR" ${CMAKE_MATCH_1})
-    string(REGEX MATCH "#define Version_Patch ([0-9]+)" MATCH_STRING ${Version_Header})
-    set("VERSION_PATCH" ${CMAKE_MATCH_1})
-    string(REGEX MATCH "#define Version_CommitID ([0-9a-fA-F]+)" MATCH_STRING ${CommitID_Header})
-    set("VERSION_COMMITID" ${CMAKE_MATCH_1})
+    if(IsSubmodule)
+        cmake_path(GET ${VERSION_FILE} ROOT_DIRECTORY VERSION_PATH) # I don't think this will work with submodules
+        execute_process(COMMAND git describe --abbrev=0 --always WORKING_DIRECTORY ${VERSION_PATH} OUTPUT_VARIABLE GIT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    elseif(NOT IsSubmodule)
+        cmake_path(GET ${VERSION_FILE} ROOT_DIRECTORY VERSION_PATH) # I don't think this will work with submodules
+        execute_process(COMMAND git ls-files -s ${VERSION_FILE} WORKING_DIRECTORY ${VERSION_PATH} OUTPUT_VARIABLE GIT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    endif(IsSubmodule)
 
-    string(REGEX REPLACE "\n$" "" "GIT_COMMIT_ID_STRIPPED" "${VERSION_COMMITID}")
-
-    string(TOUPPER "${GIT_COMMIT_ID_STRIPPED}" "GIT_COMMIT_ID_STRIPPED_CAPS")
-
-    string(CONCAT "VERSION_STRING" "${VERSION_MAJOR}" "." "${VERSION_MINOR}" "." "${VERSION_PATCH}" ":" "${GIT_COMMIT_ID_STRIPPED_CAPS}")
-
-    set("VERSION_STRING" "${VERSION_STRING}" PARENT_SCOPE)
-    #set(${Result} "${ARCH}" PARENT_SCOPE)
-    #return(${VERSION_STRING})
+    string(TOUPPER ${GIT_VERSION} Version_CommitID)
+    string(CONCAT "VERSION_STRING" "${Version_Major}" "." "${Version_Minor}" "." "${Version_Patch}" ":" "${Version_CommitID}")
+    set("OUTPUT_VERSION_STRING" "${VERSION_STRING}" PARENT_SCOPE)
 endfunction()
 endif(GIT_FOUND)
