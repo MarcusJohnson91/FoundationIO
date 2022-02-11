03d81f882c1dcd41394ead182e13f14d35473228
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2022-02-11 16:40:29 -0500

    RegisterTests.cmake: Created

---
+++ Projects/RegisterTests.cmake
@@ -0,0 +1,11 @@
+macro(RegisterTest Path Name LibraryName)
+    string(CONCAT Test_Name "Test_${Name}")
+    add_executable(${Test_Name} "${Path}/Tests/${Test_Name}.c")
+    target_link_libraries(${Test_Name} ${LibraryName})
+    set(${LibraryName}_TEST_TARGETS ${${LibraryName}_TEST_TARGETS} ${Test_Name})
+    add_test(${Test_Name} "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${Test_Name}")
+    set_tests_properties(${Test_Name} PROPERTIES TIMEOUT 15)
+endmacro(RegisterTest)
+
+add_custom_target(${LibraryName}_Tests Test DEPENDS ${${LibraryName}_TEST_TARGETS})
+add_custom_command(TARGET ${LibraryName}_Tests COMMENT "Run tests" POST_BUILD COMMAND ctest ARGS --output-on-failure WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
