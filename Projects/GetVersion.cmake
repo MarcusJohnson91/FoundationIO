986e990495786bb3813b364f80d1af08139a56ce
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2022-02-12 03:59:48 -0500

    Cmake works with submodules now

--- Projects/GetVersion.cmake
+++ Projects/GetVersion.cmake
@@ -12,24 +12,30 @@ function(GetVersionString VERSION_FILE OUTPUT_VERSION_STRING)
     set("Version_Minor" ${CMAKE_MATCH_3})
     set("Version_Patch" ${CMAKE_MATCH_4})
 
-    if(IS_DIRECTORY ".git")
-    set(GIT_ROOT_PATH ${CMAKE_CURRENT_SOURCE_DIR})
-    else
-    file(READ ".git" GIT_FILE)
-    set(GIT_ROOT_PATH "")
-    string(REPLACE "gitdir: " "" GIT_ROOT_PATH ${GIT_FILE})
-    endif(IS_DIRECTORY ".git")
+    set(Submodule_Dir "")
 
-    cmake_path(GET ${VERSION_FILE} ROOT_DIRECTORY ${GIT_ROOT_PATH})
+    if(IS_DIRECTORY "../.git")
+        set(GIT_ROOT_PATH ${CMAKE_CURRENT_SOURCE_DIR} PARENT_SCOPE)
+        set(Submodule_Dir "../")
+    else()
+        file(READ "../.git" GIT_FILE)
+        set(GIT_ROOT_PATH "" PARENT_SCOPE)
+        string(REPLACE "gitdir: " "" GIT_ROOT_PATH ${GIT_FILE})
+        string(CONCAT Submodule_Dir "${CMAKE_CURRENT_SOURCE_DIR}/../${GIT_ROOT_PATH}")
+    endif(IS_DIRECTORY "../.git")
 
-    if(IS_DIRECTORY ".git")
-        execute_process(COMMAND git ls-files -s ${VERSION_FILE} WORKING_DIRECTORY ${GIT_ROOT_PATH} OUTPUT_VARIABLE GIT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-    else
-        execute_process(COMMAND git describe --abbrev=0 --always WORKING_DIRECTORY ${GIT_ROOT_PATH} OUTPUT_VARIABLE GIT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-    endif(IS_DIRECTORY ".git")
+    set(GIT_CMD "git describe")
+    set(GIT_ARGS "--abbrev=8 --always")
+    set(GIT_VERSION "")
+    execute_process(COMMAND   git describe --abbrev=8 --always
+    WORKING_DIRECTORY ${Submodule_Dir}
+    OUTPUT_VARIABLE GIT_VERSION)
 
+    message("GIT_VERSION=${GIT_VERSION}")
+
+    set(Version_CommitID "")
     string(TOUPPER ${GIT_VERSION} Version_CommitID)
-    string(CONCAT "VERSION_STRING" "${Version_Major}" "." "${Version_Minor}" "." "${Version_Patch}" ":" "${Version_CommitID}")
-    set("OUTPUT_VERSION_STRING" VERSION_STRING PARENT_SCOPE)
+    string(CONCAT VERSION_STRING "${Version_Major}" "." "${Version_Minor}" "." "${Version_Patch}" ":" "${Version_CommitID}")
+    set(${OUTPUT_VERSION_STRING} VERSION_STRING PARENT_SCOPE)
 endfunction()
 endif(GIT_FOUND)
--- Projects/RegisterTests.cmake
+++ Projects/RegisterTests.cmake
@@ -1,11 +1,12 @@
 macro(RegisterTest Path Name LibraryName)
     string(CONCAT Test_Name "Test_${Name}")
-    add_executable(${Test_Name} "${Path}/Tests/${Test_Name}.c")
+    add_executable(${Test_Name} "${Path}/${Test_Name}.c")
     target_link_libraries(${Test_Name} ${LibraryName})
     set(${LibraryName}_TEST_TARGETS ${${LibraryName}_TEST_TARGETS} ${Test_Name})
     add_test(${Test_Name} "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${Test_Name}")
     set_tests_properties(${Test_Name} PROPERTIES TIMEOUT 15)
+
+    add_custom_command(TARGET ${LibraryName}_Tests COMMENT "Run ${LibraryName} tests" POST_BUILD COMMAND ctest ARGS --output-on-failure WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
 endmacro(RegisterTest)
 
-add_custom_target(${LibraryName}_Tests Test DEPENDS ${${LibraryName}_TEST_TARGETS})
-add_custom_command(TARGET ${LibraryName}_Tests COMMENT "Run tests" POST_BUILD COMMAND ctest ARGS --output-on-failure WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+#set(LibraryTests ${LibraryName}_Tests)
