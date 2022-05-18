48de3a235fcd01f2493113e2c29baaff130e0908
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2022-05-18 02:53:45 -0400

    Added macros to get the size of constant arrays and strings

--- Library/include/PlatformIO.h
+++ Library/include/PlatformIO.h
@@ -499,6 +499,14 @@
 #endif /* Language */
 #endif /* PlatformIO_Unused */
 
+#ifndef             PlatformIO_GetArraySize
+#define             PlatformIO_GetArraySize(Array) (sizeof(Array)/sizeof(Array[0]))
+#endif /* PlatformIO_GetArraySize */
+
+#ifndef             PlatformIO_GetStringSize
+#define             PlatformIO_GetStringSize(String) (PlatformIO_GetArraySize(String) - sizeof(String[0]))
+#endif /* PlatformIO_GetArraySize */
+
 #ifndef             PlatformIO_Concat
 #define             PlatformIO_Concat1(A, B) A##B
 #define             PlatformIO_Concat(A, B)  PlatformIO_Concat1(A, B)
