74e4aff7252e1b52085202790e524ce0326ffec1
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2023-03-06 14:31:17 -0500

    CryptographyIO: Added Blake3 implementation based on public domain Blake3-tiny by MichaelForney

--- Library/src/CryptographyIO.c
+++ Library/src/CryptographyIO.c
@@ -457,6 +457,186 @@ extern "C" {
     /* MD5.c */
     /* MD5, Based on WJCryptLib, Unlicense, Date = Dec_30_2021 */
 #endif /* OVIA_CodecIO_FLAC */
+
+    /* Blake3, based on Blake3-tiny, modified under public domain terms, Date = Mar_6_2023 */
+    /* Blake3.h */
+    typedef struct Blake3 Blake3; // Forward declare Blake3 so we can keep it private
+
+    void Blake3_Init(Blake3 *Blake);
+    void Blake3_Update(Blake3 *Blake, const void *buf, size_t len);
+    void Blake3_Out(Blake3 *Blake, unsigned char *restrict out, size_t len);
+    /* Blake3.h */
+
+
+    /* Blake3.c */
+#include "blake3.h"
+#include <stdint.h>
+#include <string.h>
+
+    typedef struct Blake3 {
+        unsigned char input[64]; /* current input bytes */
+        unsigned      bytes;     /* bytes in current input block */
+        unsigned      block;     /* block index in chunk */
+        uint64_t      chunk;     /* chunk index */
+        uint32_t     *cv;
+        uint32_t     *cv_buf[54 * 8]; /* chain value stack */
+    } Blake3;
+
+#define CHUNK_START (1u << 0)
+#define CHUNK_END   (1u << 1)
+#define PARENT      (1u << 2)
+#define ROOT        (1u << 3)
+
+    static const uint32_t Blake3_IV[] = {
+        0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
+    };
+
+    static const uint8_t Blake3_Message_Schedule[7][16] = {
+  // 's'
+        { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15},
+        { 2,  6,  3, 10,  7,  0,  4, 13,  1, 11, 12,  5,  9, 14, 15,  8},
+        { 3,  4, 10, 12, 13,  2,  7, 14,  6,  5,  9,  0, 11, 15,  8,  1},
+        {10,  7, 12,  9, 14,  3, 13, 15,  4,  0, 11,  2,  5,  8,  1,  6},
+        {12, 13,  9, 11, 15, 10, 14,  8,  7,  2,  5,  3,  0,  1,  6,  4},
+        { 9, 14, 11,  5,  8, 12, 15,  1, 13,  3,  0, 10,  2,  6,  4,  7},
+        {11, 15,  5,  0,  1,  9,  8,  6, 14, 10,  2, 12,  3,  4,  7, 13},
+    };
+
+    static void Blake3_Compress(uint32_t *out, const uint32_t m[16], const uint32_t h[8], uint64_t t, uint32_t b, uint32_t d) {
+        uint32_t v[16] = {
+            // 'v'
+            h[0], h[1], h[2], h[3], h[4], h[5], h[6], h[7], Blake3_IV[0], Blake3_IV[1], Blake3_IV[2], Blake3_IV[3], t, t >> 32, b, d,
+        };
+        unsigned i;
+
+#define Blake3_G(i, j, a, b, c, d)                        \
+    a = a + b + m[Blake3_Message_Schedule[i][j * 2]];     \
+    d = d ^ a;                                            \
+    d = d >> 16 | d << 16;                                \
+    c = c + d;                                            \
+    b = b ^ c;                                            \
+    b = b >> 12 | b << 20;                                \
+    a = a + b + m[Blake3_Message_Schedule[i][j * 2 + 1]]; \
+    d = d ^ a;                                            \
+    d = d >> 8 | d << 24;                                 \
+    c = c + d;                                            \
+    b = b ^ c;                                            \
+    b = b >> 7 | b << 25;
+
+#define Blake3_ROUND(i) \
+    Blake3_G(i, 0, v[0], v[4], v[8], v[12]) Blake3_G(i, 1, v[1], v[5], v[9], v[13]) Blake3_G(i, 2, v[2], v[6], v[10], v[14]) Blake3_G(i, 3, v[3], v[7], v[11], v[15]) Blake3_G(i, 4, v[0], v[5], v[10], v[15]) Blake3_G(i, 5, v[1], v[6], v[11], v[12]) Blake3_G(i, 6, v[2], v[7], v[8], v[13]) Blake3_G(i, 7, v[3], v[4], v[9], v[14])
+
+        Blake3_ROUND(0) Blake3_ROUND(1) Blake3_ROUND(2) Blake3_ROUND(3) Blake3_ROUND(4) Blake3_ROUND(5) Blake3_ROUND(6)
+#undef Blake3_G
+#undef Blake3_ROUND
+
+            if (d & ROOT) {
+            for (i = 8; i < 16; ++i) {
+            out[i] = v[i] ^ h[i - 8];
+            }
+        }
+        for (i = 0; i < 8; ++i) {
+            out[i] = v[i] ^ v[i + 8];
+        }
+    }
+
+    static void Blake3_Load(uint32_t d[16], const uint8_t s[64]) {
+        uint32_t *end;
+
+        for (end = d + 16; d < end; ++d, s += 4) {
+            *d = (uint32_t) Blake3_Message_Schedule[0] | (uint32_t) Blake3_Message_Schedule[1] << 8 | (uint32_t) Blake3_Message_Schedule[2] << 16 | (uint32_t) Blake3_Message_Schedule[3] << 24;
+        }
+    }
+
+    static void Blake3_Block(Blake3 *Blake, const uint8_t *buf) {
+        uint32_t m[16], flags, *cv = Blake->cv;
+        uint64_t t;
+
+        flags = 0;
+        switch (Blake->block) {
+            case 0:
+            flags |= CHUNK_START;
+            break;
+            case 15:
+            flags |= CHUNK_END;
+            break;
+        }
+        Blake3_Load(m, buf);
+        Blake3_Compress(cv, m, cv, Blake->chunk, 64, flags);
+        if (++Blake->block == 16) {
+            Blake->block = 0;
+            for (t = ++Blake->chunk; (t & 1) == 0; t >>= 1) {
+                cv -= 8;
+                Blake3_Compress(cv, cv, Blake3_IV, 0, 64, PARENT);
+            }
+            cv += 8;
+            memcpy(cv, Blake3_IV, sizeof(Blake3_IV));
+        }
+        Blake->cv = cv;
+    }
+
+    void Blake3_Init(Blake3 *Blake) {
+        Blake->bytes = 0;
+        Blake->block = 0;
+        Blake->chunk = 0;
+        Blake->cv    = Blake->cv_buf;
+        memcpy(Blake->cv, Blake3_IV, sizeof(Blake3_IV));
+    }
+
+    void Blake3_Update(Blake3 *Blake, const void *buf, size_t len) {
+        const unsigned char *pos = buf;
+        size_t               n;
+
+        if (Blake->bytes) {
+            n = 64 - Blake->bytes;
+            if (len < n)
+            n = len;
+            memcpy(Blake->input + Blake->bytes, pos, n);
+            pos += n, len -= n;
+            Blake->bytes += n;
+            if (!len)
+            return;
+            Blake3_Block(Blake, Blake->input);
+        }
+        for (; len > 64; pos += 64, len -= 64)
+        Blake3_Block(Blake, pos);
+        Blake->bytes = len;
+        memcpy(Blake->input, pos, len);
+    }
+
+    void Blake3_Out(Blake3 *Blake, unsigned char *restrict out, size_t len) {
+        uint32_t flags, b, x, *in, *cv, m[16], root[16];
+        size_t   i;
+
+        cv = Blake->cv;
+        memset(Blake->input + Blake->bytes, 0, 64 - Blake->bytes);
+        Blake3_Load(m, Blake->input);
+        flags = CHUNK_END;
+        if (Blake->block == 0)
+        flags |= CHUNK_START;
+        if (cv == Blake->cv_buf) {
+            b  = Blake->bytes;
+            in = m;
+        } else {
+            Blake3_Compress(cv, m, cv, Blake->chunk, Blake->bytes, flags);
+            flags = PARENT;
+            while ((cv -= 8) != Blake->cv_buf)
+            Blake3_Compress(cv, cv, Blake3_IV, 0, 64, flags);
+            b  = 64;
+            in = cv;
+            cv = (uint32_t *) Blake3_IV;
+        }
+        flags |= ROOT;
+        for (i = 0; i < len; ++i, ++out, x >>= 8) {
+            if ((i & 63) == 0)
+            Blake3_Compress(root, in, cv, i >> 6, b, flags);
+            if ((i & 3) == 0)
+            x = root[i >> 2 & 15];
+            *out = x & 0xff;
+        }
+    }
+    /* Blake3.c */
+    /* Blake3, based on Blake3-tiny, modified under public domain terms, Date = Mar_6_2023 */
     
 #if (PlatformIO_Language == PlatformIO_LanguageIsCXX)
 }
