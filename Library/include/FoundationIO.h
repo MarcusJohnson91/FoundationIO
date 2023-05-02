1e5b2205c265b9228bf80300ccaab1b0da46e782
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2023-05-02 16:54:02 -0400

    Moved FoundationIO.h to include

---
+++ Library/include/FoundationIO.h
@@ -0,0 +1,41 @@
+/*!
+ @header:                  FoundationIO.h
+ @author:                  Marcus Johnson
+ @copyright:               2015+
+ @SPDX-License-Identifier: Apache-2.0
+ @version:                 0.9.7
+ @brief:                   This header contains all of FoundationIO's public APIs in one easy to include header.
+ */
+
+#pragma once
+
+#ifndef FoundationIO_H
+#define FoundationIO_H
+
+#define FoundationIO_Version_Major 0
+#define FoundationIO_Version_Minor 9
+#define FoundationIO_Version_Patch 7
+
+#include "AssertIO.h"
+#include "AsynchronousIO.h"
+#include "BufferIO.h"
+#include "CollectionIO.h"
+#include "CryptographyIO.h"
+#include "FileIO.h"
+#include "GUUID.h"
+#include "MathIO.h"
+#include "NetworkIO.h"
+#include "PlatformIO.h"
+#include "RegistrationIO.h"
+#include "SortIO.h"
+#include "TestIO.h"
+#include "TextIO/TextIOTypes.h"
+#include "TextIO/CommandLineIO.h"
+#include "TextIO/FormatIO.h"
+#include "TextIO/LocalizationIO.h"
+#include "TextIO/LogIO.h"
+#include "TextIO/StringIO.h"
+#include "TextIO/StringSetIO.h"
+
+
+#endif /* FoundationIO_H */
